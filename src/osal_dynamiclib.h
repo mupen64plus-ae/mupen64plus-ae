e6676491748953fad8c3747116ae91ada34ee4ba
Richard Goedeken <Richard@fascinationsoftware.com>
2009-11-21 14:21:51 -0500

    removed unused osal_dynlib functions

--- src/osal_dynamiclib.h
+++ src/osal_dynamiclib.h
@@ -28,11 +28,7 @@ extern "C" {
 
 #include "m64p_types.h"
 
-m64p_error osal_dynlib_open(m64p_dynlib_handle *pLibHandle, const char *pccLibraryPath);
-
 void *     osal_dynlib_getproc(m64p_dynlib_handle LibHandle, const char *pccProcedureName);
-
-m64p_error osal_dynlib_close(m64p_dynlib_handle LibHandle);
 
 #ifdef __cplusplus
 }
