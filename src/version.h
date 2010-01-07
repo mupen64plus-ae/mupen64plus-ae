db0d089fe5eedcb7fb04c78dc1a51fce0e457b5b
Richard Goedeken <Richard@fascinationsoftware.com>
2010-01-06 23:44:22 -0500

    Updated RELEASE and version info for 1.99.2 tag

--- src/version.h
+++ src/version.h
@@ -27,7 +27,7 @@
 #define VERSION_H
 
 #define PLUGIN_NAME    "Mupen64Plus OpenGL Video Plugin by Rice"
-#define PLUGIN_VERSION 0x016301
+#define PLUGIN_VERSION 0x016302
 
 #define VERSION_PRINTF_SPLIT(x) (((x) >> 16) & 0xffff), (((x) >> 8) & 0xff), ((x) & 0xff)
 
