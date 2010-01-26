be6d310b6dc5a6b5a2174dff329aeae47c36d052
Richard Goedeken <Richard@fascinationsoftware.com>
2010-01-26 01:27:39 -0500

    check fread/fwrite return values and print error message, to avoid GCC warnings

--- src/liblinux/pngrw.c
+++ src/liblinux/pngrw.c
@@ -75,13 +75,15 @@ void Expand2to4( char *b4, char b2, int NumBytes )
 void user_read_data(png_structp png_read, png_bytep data, png_size_t length)
 {
     FILE *fPtr = (FILE *) png_get_io_ptr(png_read);
-    fread(data, 1, length, fPtr);
+    if (fread(data, 1, length, fPtr) != length)
+        fprintf(stderr, "Failed to read %i bytes from PNG file.\n", length);
 }
 
 void user_write_data(png_structp png_write, png_bytep data, png_size_t length)
 {
     FILE *fPtr = (FILE *) png_get_io_ptr(png_write);
-    fwrite(data, 1, length, fPtr);
+    if (fwrite(data, 1, length, fPtr) != length)
+        fprintf(stderr, "Failed to write %i bytes to PNG file.\n", length);
 }
 
 void user_flush_data(png_structp png_read)
