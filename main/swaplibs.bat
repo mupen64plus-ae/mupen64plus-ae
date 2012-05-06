8528be09799124850c4cefc815e010c08cc04dab
lioncash <mathew1900@hotmail.com>
2012-05-06 02:09:59 -0400

    Add Windows equivalent of the swaplibs script.

---
+++ main/swaplibs.bat
@@ -0,0 +1,9 @@
+del libs/armeabi/libcore.so
+del obj/local/armeabi/libcore.so
+del libs/armeabi-v7a/libcore.so
+del obj/local/armeabi-v7a/libcore.so
+copy ../mupen64plus-core-ARCHs/libs/armeabi/libcore.so libs/armeabi/
+copy ../mupen64plus-core-ARCHs/libs/armeabi/libcore.so obj/local/armeabi/
+copy ../mupen64plus-core-ARCHs/libs/armeabi-v7a/libcore.so libs/armeabi-v7a/
+copy ../mupen64plus-core-ARCHs/libs/armeabi-v7a/libcore.so obj/local/armeabi-v7a/
+
