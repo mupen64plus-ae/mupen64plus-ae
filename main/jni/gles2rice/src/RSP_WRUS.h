2a0ccd9122dc41aad942d6ec251b9d188b9a2a32
lioncash <mathew1900@hotmail.com>
2012-05-08 22:45:10 -0400

    replace u32 with the more informative uint32 (programmers should know the they mean the same thing, but hey, a little syntactic sugar never hurt anybody).

--- main/jni/gles2rice/src/RSP_WRUS.h
+++ main/jni/gles2rice/src/RSP_WRUS.h
@@ -21,26 +21,26 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 void RSP_Vtx_WRUS(Gfx *gfx)
 {
-	u32 dwAddr	 = RSPSegmentAddr(gfx->words.cmd1);
-	u32 dwV0	 = ((gfx->words.cmd0 >>16 ) & 0xff) / 5;
-	u32 dwN		 =  (gfx->words.cmd0 >>9  ) & 0x7f;
-	u32 dwLength =  (gfx->words.cmd0      ) & 0x1ff;
+    uint32 dwAddr    = RSPSegmentAddr(gfx->words.cmd1);
+    uint32 dwV0      = ((gfx->words.cmd0 >>16 ) & 0xff) / 5;
+    uint32 dwN		 =  (gfx->words.cmd0 >>9  ) & 0x7f;
+    uint32 dwLength  =  (gfx->words.cmd0      ) & 0x1ff;
 
-	LOG_UCODE("    Address [0x%08x], v0: [%d], Num: [%d], Length: [0x%04x]", dwAddr, dwV0, dwN, dwLength);
+    LOG_UCODE("    Address [0x%08x], v0: [%d], Num: [%d], Length: [0x%04x]", dwAddr, dwV0, dwN, dwLength);
 
-	if (dwV0 >= 32)
-		dwV0 = 31;
-	
-	if ((dwV0 + dwN) > 32)
-	{
-		TRACE0("Warning, attempting to load into invalid vertex positions");
-		dwN = 32 - dwV0;
-	}
+    if (dwV0 >= 32)
+        dwV0 = 31;
+    
+    if ((dwV0 + dwN) > 32)
+    {
+        TRACE0("Warning, attempting to load into invalid vertex positions");
+        dwN = 32 - dwV0;
+    }
 
-	ProcessVertexData(dwAddr, dwV0, dwN);
+    ProcessVertexData(dwAddr, dwV0, dwN);
 
-	status.dwNumVertices += dwN;
-	DisplayVertexInfo(dwAddr, dwV0, dwN);
+    status.dwNumVertices += dwN;
+    DisplayVertexInfo(dwAddr, dwV0, dwN);
 }
 
 #endif //RSP_WRUS_H__
