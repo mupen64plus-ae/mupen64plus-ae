1e75b4b2423ff0818c4e54d038fbb19bc4e76ab3
lioncash <mathew1900@hotmail.com>
2012-11-09 08:24:44 -0500

    Fix a javadoc @see tag.

--- src/paulscode/android/mupen64plusae/input/map/InputMap.java
+++ src/paulscode/android/mupen64plusae/input/map/InputMap.java
@@ -21,6 +21,7 @@ package paulscode.android.mupen64plusae.input.map;
 
 import paulscode.android.mupen64plusae.InputMapPreference;
 import paulscode.android.mupen64plusae.input.AbstractController;
+import paulscode.android.mupen64plusae.input.PeripheralController;
 import paulscode.android.mupen64plusae.input.provider.AbstractProvider;
 import paulscode.android.mupen64plusae.util.SafeMethods;
 import paulscode.android.mupen64plusae.util.SubscriptionManager;
