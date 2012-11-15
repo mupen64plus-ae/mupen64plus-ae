mupen64plus-ae/mupen64plus-ae

commit 5588f36c9773bccd5982541a2a7208a50cc81f5a

Tweaked input architecture: cleaner, safer, and more intuitive
A little more organized, more parallelism between classes, eliminated
code duplication.  Changed name from 'transform' to 'provider' and more
initialization now happens in the constructor rather than relying on
client code choreography.  This was one of the little things bugging me
about the new input architecture.  Smells better now.

src/paulscode/android/mupen64plusae/input/provider/AbstractProvider.java
@@ -64,9 +64,11 @@ public abstract class AbstractProvider
     public static String getInputName( int inputCode )
     {
         // TODO: Localize strings.
+        boolean isHoneycombMR1 = Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB_MR1;
+
         if( inputCode > 0 )
         {
-            if( Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB_MR1 )
+            if( isHoneycombMR1 )
                 return "KEYCODE_" + inputCode;
             else
                 return KeyEvent.keyCodeToString( inputCode );
@@ -77,7 +79,7 @@ public abstract class AbstractProvider
             String direction = inputToAxisDirection( inputCode )
                     ? " (+)"
                     : " (-)";
-            if( Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB_MR1 )
+            if( isHoneycombMR1 )
                 return "AXIS_" + axis + direction;
             else
                 return MotionEvent.axisToString( axis ) + direction;

src/paulscode/android/mupen64plusae/input/provider/AxisProvider.java
@@ -20,24 +20,40 @@
 package paulscode.android.mupen64plusae.input.provider;
 
 import android.annotation.TargetApi;
+import android.os.Build;
 import android.view.InputDevice;
 import android.view.InputDevice.MotionRange;
 import android.view.MotionEvent;
 import android.view.View;
 
 @TargetApi( 12 )
-public class AxisProvider extends KeyProvider implements View.OnGenericMotionListener
+public class AxisProvider extends AbstractProvider implements View.OnGenericMotionListener
 {
     private int[] mInputCodes;
     
     private static final int DEFAULT_NUM_INPUTS = 128;
     
-    public AxisProvider()
+    public static AxisProvider create( View view )
     {
-        // By default, listen to all possible gamepad axes
+        // Use a factory method for API safety
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1 )
+            return new AxisProvider( view );
+        else
+            return null;
+    }
+    
+    private AxisProvider( View view )
+    {
+        // By default, provide data from all possible axes
         mInputCodes = new int[DEFAULT_NUM_INPUTS];
         for( int i = 0; i < mInputCodes.length; i++ )
             mInputCodes[i] = -( i + 1 );
+        
+        // Connect the input source
+        view.setOnGenericMotionListener( this );
+        
+        // Request focus for proper listening
+        view.requestFocus();
     }
     
     public void setInputCodeFilter( int[] inputCodeFilter )

src/paulscode/android/mupen64plusae/GameImplementation.java
@@ -24,6 +24,7 @@ import java.io.File;
 import paulscode.android.mupen64plusae.input.PeripheralController;
 import paulscode.android.mupen64plusae.input.TouchscreenController;
 import paulscode.android.mupen64plusae.input.map.VisibleTouchMap;
+import paulscode.android.mupen64plusae.input.provider.AxisProvider;
 import paulscode.android.mupen64plusae.input.provider.KeyProvider;
 import paulscode.android.mupen64plusae.input.provider.KeyProvider.ImeFormula;
 import paulscode.android.mupen64plusae.util.FileUtil;
@@ -58,7 +59,8 @@ public class GameImplementation implements View.OnKeyListener
     private TouchscreenView mTouchscreenView;
     @SuppressWarnings( "unused" )
     private TouchscreenController mTouchscreenController;
-    private KeyProvider mKeyTransform;
+    private KeyProvider mKeyProvider;
+    private AxisProvider mAxisProvider;
     private PeripheralController mPeripheralController1;
     private PeripheralController mPeripheralController2;
     private PeripheralController mPeripheralController3;
@@ -125,7 +127,7 @@ public class GameImplementation implements View.OnKeyListener
         initPeripherals();
         Vibrator vibrator = (Vibrator) mActivity.getSystemService( Context.VIBRATOR_SERVICE );
         
-        // Override the peripheral controller key listener, to add some extra functionality
+        // Override the key provider, to add some extra functionality
         mSdlSurface.setOnKeyListener( this );
         
         // Synchronize the interface to the emulator core
@@ -234,9 +236,9 @@ public class GameImplementation implements View.OnKeyListener
                         || keyCode == KeyEvent.KEYCODE_VOLUME_DOWN || keyCode == KeyEvent.KEYCODE_VOLUME_MUTE ) )
             return false;
         
-        // Send everything else to the peripheral controllers via the transform
-        else if( mKeyTransform != null )
-            return mKeyTransform.onKey( view, keyCode, event );
+        // Send everything else to the key provider
+        else if( mKeyProvider != null )
+            return mKeyProvider.onKey( view, keyCode, event );
         
         // Let Android handle whatever remains
         else
@@ -267,32 +269,33 @@ public class GameImplementation implements View.OnKeyListener
     {
         if( Globals.userPrefs.isInputEnabled )
         {
-            // Create the common transform used for all peripheral controllers
-            mKeyTransform = PeripheralController.buildTransform( mSdlSurface, ImeFormula.DEFAULT );
+            // Create the input providers shared among all peripheral controllers
+            mKeyProvider = new KeyProvider( mSdlSurface, ImeFormula.DEFAULT );
+            mAxisProvider = AxisProvider.create( mSdlSurface );
             
             // Create the peripheral controllers for players 1-4
             if( Globals.userPrefs.inputMap1.isEnabled() )
             {
                 mPeripheralController1 = new PeripheralController( Globals.userPrefs.inputMap1,
-                        mKeyTransform );
+                        mKeyProvider, mAxisProvider );
                 mPeripheralController1.setPlayerNumber( 1 );
             }
             if( Globals.userPrefs.inputMap2.isEnabled() )
             {
                 mPeripheralController2 = new PeripheralController( Globals.userPrefs.inputMap2,
-                        mKeyTransform );
+                        mKeyProvider, mAxisProvider );
                 mPeripheralController2.setPlayerNumber( 2 );
             }
             if( Globals.userPrefs.inputMap3.isEnabled() )
             {
                 mPeripheralController3 = new PeripheralController( Globals.userPrefs.inputMap3,
-                        mKeyTransform );
+                        mKeyProvider, mAxisProvider );
                 mPeripheralController3.setPlayerNumber( 3 );
             }
             if( Globals.userPrefs.inputMap4.isEnabled() )
             {
                 mPeripheralController4 = new PeripheralController( Globals.userPrefs.inputMap4,
-                        mKeyTransform );
+                        mKeyProvider, mAxisProvider );
                 mPeripheralController4.setPlayerNumber( 4 );
             }
         }
@@ -352,13 +355,13 @@ public class GameImplementation implements View.OnKeyListener
         CharSequence title = mActivity.getText( R.string.ingameSave_title );
         CharSequence hint = mActivity.getText( R.string.gameImplementation_saveHint );
         Prompt.promptText( mActivity, title, null, hint, new OnTextListener()
-                {
-                    @Override
-                    public void onText( CharSequence text )
-                    {
-                        saveStateToFile( text.toString() );
-                    }
-                } );
+        {
+            @Override
+            public void onText( CharSequence text )
+            {
+                saveStateToFile( text.toString() );
+            }
+        } );
     }
     
     private void saveStateToFile( final String filename )
@@ -367,20 +370,20 @@ public class GameImplementation implements View.OnKeyListener
         if( file.exists() )
         {
             String title = mActivity.getString( R.string._confirmation );
-            String message = mActivity.getString( R.string.gameImplementation_confirmFile, filename );
-            Prompt.promptConfirm( mActivity, title, message,
-                    new OnClickListener()
+            String message = mActivity
+                    .getString( R.string.gameImplementation_confirmFile, filename );
+            Prompt.promptConfirm( mActivity, title, message, new OnClickListener()
+            {
+                @Override
+                public void onClick( DialogInterface dialog, int which )
+                {
+                    if( which == DialogInterface.BUTTON_POSITIVE )
                     {
-                        @Override
-                        public void onClick( DialogInterface dialog, int which )
-                        {
-                            if( which == DialogInterface.BUTTON_POSITIVE )
-                            {
-                                Log.i( "GameImplementation", "Overwriting file " + filename );
-                                NativeMethods.fileSaveEmulator( file.getAbsolutePath() );
-                            }
-                        }
-                    } );
+                        Log.i( "GameImplementation", "Overwriting file " + filename );
+                        NativeMethods.fileSaveEmulator( file.getAbsolutePath() );
+                    }
+                }
+            } );
         }
         else
         {

src/paulscode/android/mupen64plusae/InputMapPreference.java
@@ -55,7 +55,8 @@ public class InputMapPreference extends DialogPreference implements AbstractProv
     private View mToggleWidget;
     private TextView mFeedbackText;
     private Button[] mN64Button = new Button[InputMap.NUM_N64INPUTS];
-    private KeyProvider mTransform;
+    private KeyProvider mKeyProvider = null;
+    private AxisProvider mAxisProvider = null;
     
     public InputMapPreference( Context context, AttributeSet attrs )
     {
@@ -79,7 +80,6 @@ public class InputMapPreference extends DialogPreference implements AbstractProv
         ( (Checkable) mToggleWidget ).setChecked( mMap.isEnabled() );
     }
     
-    @TargetApi( 12 )
     @Override
     protected void onBindDialogView( View view )
     {
@@ -121,27 +121,10 @@ public class InputMapPreference extends DialogPreference implements AbstractProv
             b.setOnLongClickListener( this );
         }
         
-        // Set up input listening
-        if( Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB_MR1 )
-        {
-            // For Android 3.0 and below, we can only listen to keyboards
-            mTransform = new KeyProvider();
-        }
-        else
-        {
-            // For Android 3.1 and above, we can also listen to gamepads, mice, etc.
-            AxisProvider transform = new AxisProvider();
-            
-            // Connect the extra upstream end of the transform
-            view.setOnGenericMotionListener( transform );
-            mTransform = transform;
-        }
-        
-        // Set the formula for decoding special analog IMEs
-        mTransform.setImeFormula( ImeFormula.DEFAULT );
-        
-        // Request focus for proper listening
-        view.requestFocus();
+        // Setup axis listening
+        mAxisProvider = AxisProvider.create( view );
+        if( mAxisProvider != null )
+            mAxisProvider.registerListener( this );
         
         // Refresh the dialog view
         updateViews();
@@ -152,11 +135,9 @@ public class InputMapPreference extends DialogPreference implements AbstractProv
     {
         super.onPrepareDialogBuilder( builder );
         
-        // Connect the upstream end of the transform
-        builder.setOnKeyListener( mTransform );
-        
-        // Connect the downstream end of the transform
-        mTransform.registerListener( this );
+        // Setup key listening
+        mKeyProvider = new KeyProvider( builder, ImeFormula.DEFAULT );
+        mKeyProvider.registerListener( this );
     }
     
     @Override
@@ -168,6 +149,12 @@ public class InputMapPreference extends DialogPreference implements AbstractProv
         
         // Refresh the biases next time the dialog opens
         mStrengthBiases = null;
+        
+        // Disconnect the listeners
+        if( mAxisProvider != null )
+            mAxisProvider.unregisterListener( this );
+        if( mKeyProvider != null )
+            mKeyProvider.unregisterListener( this );
     }
     
     @Override
@@ -240,7 +227,7 @@ public class InputMapPreference extends DialogPreference implements AbstractProv
         }
         // Else, handle the mapping buttons in the dialog (never unmap, would confuse user)
         else if( mInputCodeToBeMapped != 0 )
-        {            
+        {
             // Find the button that was touched and map it
             for( int i = 0; i < mN64Button.length; i++ )
             {
@@ -259,7 +246,7 @@ public class InputMapPreference extends DialogPreference implements AbstractProv
         // Find the Button view that was long-touched and unmap it
         for( int i = 0; i < mN64Button.length; i++ )
         {
-            if( view.equals( mN64Button[i]) )
+            if( view.equals( mN64Button[i] ) )
                 mMap.unmapInput( i );
         }
         

src/paulscode/android/mupen64plusae/SDLSurface.java
@@ -30,7 +30,6 @@ import javax.microedition.khronos.egl.EGLSurface;
 import paulscode.android.mupen64plusae.input.map.VisibleTouchMap;
 import paulscode.android.mupen64plusae.util.Notifier;
 import paulscode.android.mupen64plusae.util.SafeMethods;
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -63,7 +62,6 @@ public class SDLSurface extends SurfaceView implements SurfaceHolder.Callback
     private EGLDisplay mEGLDisplay;
     
     // Startup
-    @TargetApi( 12 )
     public SDLSurface( Context context, AttributeSet attribs )
     {
         super( context, attribs );

src/paulscode/android/mupen64plusae/input/PeripheralController.java
@@ -23,52 +23,19 @@ import paulscode.android.mupen64plusae.input.map.InputMap;
 import paulscode.android.mupen64plusae.input.provider.AbstractProvider;
 import paulscode.android.mupen64plusae.input.provider.AxisProvider;
 import paulscode.android.mupen64plusae.input.provider.KeyProvider;
-import paulscode.android.mupen64plusae.input.provider.KeyProvider.ImeFormula;
-import android.annotation.TargetApi;
-import android.os.Build;
-import android.view.View;
 
 public class PeripheralController extends AbstractController implements AbstractProvider.Listener,
         InputMap.Listener
 {
     private InputMap mInputMap;
-    private KeyProvider mTransform;
+    private KeyProvider mKeyProvider;
+    private AxisProvider mAxisProvider;
     protected float mAxisFractionXpos;
     protected float mAxisFractionXneg;
     protected float mAxisFractionYpos;
     protected float mAxisFractionYneg;
     
-    @TargetApi( 12 )
-    public static KeyProvider buildTransform( View view, ImeFormula formula )
-    {
-        KeyProvider transform;
-        
-        // Set up input listening
-        if( Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB_MR1 )
-        {
-            // For Android 3.0 and below, we can only listen to keyboards
-            transform = new KeyProvider();
-        }
-        else
-        {
-            // For Android 3.1 and above, we can also listen to gamepads, mice, etc.
-            AxisProvider kaTransform = new AxisProvider();
-            
-            // Connect the extra upstream end of the transform
-            view.setOnGenericMotionListener( kaTransform );
-            transform = kaTransform;
-        }
-        
-        // Set the formula for decoding special analog IMEs
-        transform.setImeFormula( ImeFormula.DEFAULT );
-        
-        // Connect the upstream end of the transform
-        view.setOnKeyListener( transform );
-        
-        return transform;
-    }
-    
-    public PeripheralController( InputMap inputMap, KeyProvider transform )
+    public PeripheralController( InputMap inputMap, KeyProvider keyProvider, AxisProvider axisProvider )
     {
         // Assign the map and listen for changes
         mInputMap = inputMap;
@@ -78,11 +45,15 @@ public class PeripheralController extends AbstractController implements Abstract
             mInputMap.registerListener( this );
         }
         
-        // Assign the transform
-        mTransform = transform;
+        // Assign the providers
+        mKeyProvider = keyProvider;
+        mAxisProvider = axisProvider;
         
-        // Connect the downstream end of the transform
-        mTransform.registerListener( this );
+        // Connect the providers' destination
+        if( mKeyProvider != null )
+            mKeyProvider.registerListener( this );
+        if( mAxisProvider != null )
+            mAxisProvider.registerListener( this );
     }
     
     @Override
@@ -118,10 +89,8 @@ public class PeripheralController extends AbstractController implements Abstract
     public void onMapChanged( InputMap map )
     {
         // If the button/axis mappings change, update the transform's listening filter
-        if( mTransform != null && mTransform instanceof AxisProvider )
-        {
-            ( (AxisProvider) mTransform ).setInputCodeFilter( map.getMappedInputCodes() );
-        }
+        if( mAxisProvider != null )
+            mAxisProvider.setInputCodeFilter( map.getMappedInputCodes() );
     }
     
     public boolean apply( int inputCode, float strength )

src/paulscode/android/mupen64plusae/input/provider/KeyProvider.java
@@ -19,6 +19,7 @@
  */
 package paulscode.android.mupen64plusae.input.provider;
 
+import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.view.KeyEvent;
 import android.view.View;
@@ -33,19 +34,25 @@ public class KeyProvider extends AbstractProvider implements View.OnKeyListener,
     
     private ImeFormula mImeFormula;
     
-    public KeyProvider()
+    public KeyProvider( View view, ImeFormula formula )
     {
-        setImeFormula( ImeFormula.DEFAULT );
+        // Assign the IME decoding formula
+        mImeFormula = formula;
+        
+        // Connect the input source
+        view.setOnKeyListener( this );
+        
+        // Request focus for proper listening
+        view.requestFocus();
     }
     
-    public ImeFormula getImeFormula()
+    public KeyProvider( Builder builder, ImeFormula formula )
     {
-        return mImeFormula;
-    }
-    
-    public void setImeFormula( ImeFormula imeFormula )
-    {
-        mImeFormula = imeFormula;
+        // Assign the IME decoding formula
+        mImeFormula = formula;
+        
+        // Connect the input source
+        builder.setOnKeyListener( this );
     }
     
     @Override
@@ -78,7 +85,7 @@ public class KeyProvider extends AbstractProvider implements View.OnKeyListener,
         else
         {
             // Analog axis changed state, decode using IME-specific formula
-            switch( getImeFormula() )
+            switch( mImeFormula )
             {
                 case DEFAULT:
                 case USB_BT_JOYSTICK_CENTER:

src/paulscode/android/mupen64plusae/input/provider/SensorProvider.java
@@ -15,11 +15,10 @@
  * See the GNU General Public License for more details. You should have received a copy of the GNU
  * General Public License along with Mupen64PlusAE. If not, see <http://www.gnu.org/licenses/>.
  * 
- * Authors: TODO: Implement sensor controller, if desired
+ * Authors: TODO: Implement sensor provider, if desired
  */
 package paulscode.android.mupen64plusae.input.provider;
 
-import paulscode.android.mupen64plusae.NativeMethods;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -34,9 +33,5 @@ public class SensorProvider extends AbstractProvider implements SensorEventListe
     @Override
     public void onSensorChanged( SensorEvent event )
     {
-        if( event.sensor.getType() == Sensor.TYPE_ACCELEROMETER )
-        {
-            NativeMethods.onAccel( event.values[0], event.values[1], event.values[2] );
-        }
     }
 }

commit 953b2a3fe4a697a66c32991c0d7ffb8a10dc4de6

Fixed API-related issues and a nasty little bug in AbstractProvider
Bug introduced in 5588f36c9773bccd5982541a2a7208a50cc81f5a:
 isHoneyComb_MR1 conditional got flipped

src/paulscode/android/mupen64plusae/input/provider/AbstractProvider.java
@@ -115,20 +115,20 @@ public abstract class AbstractProvider
         if( inputCode > 0 )
         {
             if( Globals.IS_HONEYCOMB_MR1 )
-                return "KEYCODE_" + inputCode;
-            else
                 return KeyEvent.keyCodeToString( inputCode );
+            else
+                return "KEYCODE_" + inputCode;
         }
         else if( inputCode < 0 )
         {
-            int axis = AxisProvider.inputToAxisCode( inputCode );
-            String direction = AxisProvider.inputToAxisDirection( inputCode )
+            int axis = inputToAxisCode( inputCode );
+            String direction = inputToAxisDirection( inputCode )
                     ? " (+)"
                     : " (-)";
             if( Globals.IS_HONEYCOMB_MR1 )
-                return "AXIS_" + axis + direction;
-            else
                 return MotionEvent.axisToString( axis ) + direction;
+            else
+                return "AXIS_" + axis + direction;
         }
         else
             return "NULL";
@@ -146,6 +146,44 @@ public abstract class AbstractProvider
         return getInputName( inputCode ) + ( inputCode == 0
                 ? ""
                 : String.format( " %4.2f", strength ) );
+    }    
+    
+    /**
+     * Convert an Android axis code to a universal input code.
+     * 
+     * @param axisCode The Android axis code.
+     * @param positiveDirection Set true for positive Android axis, false for negative Android axis.
+     * @return The corresponding universal input code.
+     */
+    protected static int axisToInputCode( int axisCode, boolean positiveDirection )
+    {
+        // Axis codes are encoded to negative values (versus buttons which are positive). Axis codes
+        // are bit shifted by one so that the lowest bit can encode axis direction.
+        return -( ( axisCode ) * 2 + ( positiveDirection
+                ? 1
+                : 2 ) );
+    }
+    
+    /**
+     * Convert a universal input code to an Android axis code.
+     * 
+     * @param inputCode The universal input code.
+     * @return The corresponding Android axis code.
+     */
+    protected static int inputToAxisCode( int inputCode )
+    {
+        return ( -inputCode - 1 ) / 2;
+    }
+    
+    /**
+     * Convert a universal input code to an Android axis direction.
+     * 
+     * @param inputCode The universal input code.
+     * @return True if the input code represents positive Android axis direction, false otherwise.
+     */
+    protected static boolean inputToAxisDirection( int inputCode )
+    {
+        return ( ( -inputCode ) % 2 ) == 1;
     }
     
     /**

src/paulscode/android/mupen64plusae/input/provider/AxisProvider.java
@@ -28,7 +28,6 @@ import android.view.View;
 /**
  * A class for transforming Android MotionEvent inputs into a common format.
  */
-@TargetApi( 12 )
 public class AxisProvider extends AbstractProvider implements View.OnGenericMotionListener
 {
     /** The input codes to listen for. */
@@ -42,6 +41,7 @@ public class AxisProvider extends AbstractProvider implements View.OnGenericMoti
      * 
      * @param view The view receiving MotionEvent data.
      */
+    @TargetApi( 12 )
     public AxisProvider( View view )
     {
         // By default, provide data from all possible axes
@@ -72,6 +72,7 @@ public class AxisProvider extends AbstractProvider implements View.OnGenericMoti
      * @see android.view.View.OnGenericMotionListener#onGenericMotion(android.view.View,
      * android.view.MotionEvent)
      */
+    @TargetApi( 12 )
     @Override
     public boolean onGenericMotion( View v, MotionEvent event )
     {
@@ -105,43 +106,5 @@ public class AxisProvider extends AbstractProvider implements View.OnGenericMoti
         notifyListeners( mInputCodes, strengths, 0 );
         
         return true;
-    }
-    
-    /**
-     * Convert an Android axis code to a universal input code.
-     * 
-     * @param axisCode The Android axis code.
-     * @param positiveDirection Set true for positive Android axis, false for negative Android axis.
-     * @return The corresponding universal input code.
-     */
-    protected static int axisToInputCode( int axisCode, boolean positiveDirection )
-    {
-        // Axis codes are encoded to negative values (versus buttons which are positive). Axis codes
-        // are bit shifted by one so that the lowest bit can encode axis direction.
-        return -( ( axisCode ) * 2 + ( positiveDirection
-                ? 1
-                : 2 ) );
-    }
-    
-    /**
-     * Convert a universal input code to an Android axis code.
-     * 
-     * @param inputCode The universal input code.
-     * @return The corresponding Android axis code.
-     */
-    protected static int inputToAxisCode( int inputCode )
-    {
-        return ( -inputCode - 1 ) / 2;
-    }
-    
-    /**
-     * Convert a universal input code to an Android axis direction.
-     * 
-     * @param inputCode The universal input code.
-     * @return True if the input code represents positive Android axis direction, false otherwise.
-     */
-    protected static boolean inputToAxisDirection( int inputCode )
-    {
-        return ( ( -inputCode ) % 2 ) == 1;
     }
 }