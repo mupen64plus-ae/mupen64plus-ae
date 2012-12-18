7d9aca8884ca5b07847b37c62e871cbda2cf29b1
lioncash <mathew1900@hotmail.com>
2012-12-18 18:58:12 -0500

    Fix the todo message in CheatsMenuHandler.java. Shift-JIS encoding mangled it.

--- src/paulscode/android/mupen64plusae/CheatsMenuHandler.java
+++ src/paulscode/android/mupen64plusae/CheatsMenuHandler.java
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Mupen64PlusAE, an N64 emulator for the Android platform
  * 
  * Copyright (C) 2012 Paul Lamb
@@ -111,7 +111,7 @@ public class CheatsMenuHandler implements OnPreferenceClickListener, OnPreferenc
                 else
                 {
                     // TODO: This is not region friendly. Consider Japanese for example. "Cheats for Mario 64"
-                    // in Japanese would be "Mario 64�̃`�[�g" (ie. The text is AFTER the name of the ROM)
+                    // in Japanese would be "Mario 64のチート" (ie. The text is AFTER the name of the ROM)
                     cheatsScreen.setTitle( mActivity.getString( R.string.cheats_titleFor ) + ROM_name );
                 }
                 
