dafc4f8ce98fe0d0e8e20d7dd4c81feee5869f6f
littleguy77 <mattlichter@live.com>
2013-01-18 08:16:01 -0500

    cleanup: Updated all java header comments to GPL v3.
    Jautodoc preferences in ./guidelines/config_files/

--- src/paulscode/android/mupen64plusae/GameSurface.java
+++ src/paulscode/android/mupen64plusae/GameSurface.java
@@ -1,21 +1,22 @@
 /**
  * Mupen64PlusAE, an N64 emulator for the Android platform
  * 
- * Copyright (C) 2012 Paul Lamb
+ * Copyright (C) 2013 Paul Lamb
  * 
  * This file is part of Mupen64PlusAE.
  * 
  * Mupen64PlusAE is free software: you can redistribute it and/or modify it under the terms of the
- * GNU General Public License as published by the Free Software Foundation, either version 2 of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  * 
  * Mupen64PlusAE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  * 
- * See the GNU General Public License for more details. You should have received a copy of the GNU
- * General Public License along with Mupen64PlusAE. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with Mupen64PlusAE. If
+ * not, see <http://www.gnu.org/licenses/>.
  * 
- * Authors: paulscode, lioncash
+ * Authors: Paul Lamb, littleguy77
  */
 package paulscode.android.mupen64plusae;
 
