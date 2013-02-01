mupen64plus-ae/mupen64plus-ae

commit 59ca551bd27f1db09dca295c2e9f2c28b1da85bd

front: Move some java code functionality into xml resources.

src/paulscode/android/mupen64plusae/MenuActivity.java
@@ -239,10 +239,6 @@ public class MenuActivity extends PreferenceActivity implements OnPreferenceClic
         PrefUtil.enablePreference( this, TOUCHSCREEN_SIZE, user.isTouchscreenEnabled
                 && !user.isTouchscreenCustom );
         
-        // Update the summary text for all relevant preferences
-        for( String key : sharedPreferences.getAll().keySet() )
-            PrefUtil.refreshSummary( this, key );
-        
         // Update the summary text in a particular way for ACRA user info
         EditTextPreference pref = (EditTextPreference) findPreference( ACRA_USER_EMAIL );
         String value = pref.getText();

src/paulscode/android/mupen64plusae/util/PrefUtil.java
@@ -20,7 +20,6 @@
  */
 package paulscode.android.mupen64plusae.util;
 
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
@@ -53,13 +52,5 @@ public class PrefUtil
         Preference child = activity.findPreference( keyChild );
         if( parent instanceof PreferenceGroup && child != null )
             ( (PreferenceGroup) parent ).removePreference( child );
-    }
-    
-    @SuppressWarnings( "deprecation" )
-    public static void refreshSummary( PreferenceActivity activity, String key )
-    {
-        Preference preference = activity.findPreference( key );
-        if( preference instanceof ListPreference )
-            preference.setSummary( ( (ListPreference) preference ).getEntry() );
     }
 }

res/values/strings-no-translate.xml
@@ -26,5 +26,6 @@
     
     <!-- Aliases to other strings -->
     <string name="localeOverride_title">@string/actionSetLocale_title</string>
+    <string name="selectedValue">%1$s</string>
 
 </resources>

res/xml/preferences.xml
@@ -91,6 +91,7 @@
                             android:entries="@array/touchscreenLayout_entries"
                             android:entryValues="@array/touchscreenLayout_values"
                             android:key="touchscreenLayout"
+                            android:summary="@string/selectedValue"
                             android:title="@string/touchscreenLayout_title" />
                         <ListPreference
                             android:defaultValue="@string/touchscreenSize_default"
@@ -98,6 +99,7 @@
                             android:entries="@array/touchscreenSize_entries"
                             android:entryValues="@array/touchscreenSize_values"
                             android:key="touchscreenSize"
+                            android:summary="@string/selectedValue"
                             android:title="@string/touchscreenSize_title" />
 
                         <paulscode.android.mupen64plusae.persistent.PathPreference
@@ -122,6 +124,7 @@
                             android:entries="@array/touchscreenRefresh_entries"
                             android:entryValues="@array/touchscreenRefresh_values"
                             android:key="touchscreenRefresh"
+                            android:summary="@string/selectedValue"
                             android:title="@string/touchscreenRefresh_title" />
                     </PreferenceScreen>
                     <PreferenceScreen
@@ -140,6 +143,7 @@
                             android:entries="@array/touchpadLayout_entries"
                             android:entryValues="@array/touchpadLayout_values"
                             android:key="touchpadLayout"
+                            android:summary="@string/selectedValue"
                             android:title="@string/touchpadLayout_title" />
                     </PreferenceScreen>
 
@@ -224,18 +228,21 @@
                     android:entries="@array/videoOrientation_entries"
                     android:entryValues="@array/videoOrientation_values"
                     android:key="videoOrientation"
+                    android:summary="@string/selectedValue"
                     android:title="@string/videoOrientation_title" />
                 <ListPreference
                     android:defaultValue="@string/videoFpsRefresh_default"
                     android:entries="@array/videoFpsRefresh_entries"
                     android:entryValues="@array/videoFpsRefresh_values"
                     android:key="videoFpsRefresh"
+                    android:summary="@string/selectedValue"
                     android:title="@string/videoFpsRefresh_title" />
                 <ListPreference
                     android:defaultValue="@string/videoHardwareType_default"
                     android:entries="@array/videoHardwareType_entries"
                     android:entryValues="@array/videoHardwareType_values"
                     android:key="videoHardwareType"
+                    android:summary="@string/selectedValue"
                     android:title="@string/videoHardwareType_title" />
 
                 <CheckBoxPreference
@@ -262,6 +269,7 @@
                         android:entries="@array/gles2N64Frameskip_entries"
                         android:entryValues="@array/gles2N64Frameskip_values"
                         android:key="gles2N64Frameskip"
+                        android:summary="@string/selectedValue"
                         android:title="@string/gles2N64Frameskip_title" />
 
                     <CheckBoxPreference
@@ -343,6 +351,7 @@
                     android:entries="@array/audioResampleAlg_entries"
                     android:entryValues="@array/audioResampleAlg_values"
                     android:key="audioResampleAlg"
+                    android:summary="@string/selectedValue"
                     android:title="@string/audioResampleAlg_title" />
             </PreferenceScreen>
             <PreferenceScreen
@@ -355,30 +364,35 @@
                     android:entries="@array/pluginVideo_entries"
                     android:entryValues="@array/pluginVideo_values"
                     android:key="pluginVideo"
+                    android:summary="@string/selectedValue"
                     android:title="@string/pluginVideo_title" />
                 <ListPreference
                     android:defaultValue="@string/pluginInput_default"
                     android:entries="@array/pluginInput_entries"
                     android:entryValues="@array/pluginInput_values"
                     android:key="pluginInput"
+                    android:summary="@string/selectedValue"
                     android:title="@string/pluginInput_title" />
                 <ListPreference
                     android:defaultValue="@string/pluginAudio_default"
                     android:entries="@array/pluginAudio_entries"
                     android:entryValues="@array/pluginAudio_values"
                     android:key="pluginAudio"
+                    android:summary="@string/selectedValue"
                     android:title="@string/pluginAudio_title" />
                 <ListPreference
                     android:defaultValue="@string/pluginRsp_default"
                     android:entries="@array/pluginRsp_entries"
                     android:entryValues="@array/pluginRsp_values"
                     android:key="pluginRsp"
+                    android:summary="@string/selectedValue"
                     android:title="@string/pluginRsp_title" />
                 <ListPreference
                     android:defaultValue="@string/pluginCore_default"
                     android:entries="@array/pluginCore_entries"
                     android:entryValues="@array/pluginCore_values"
                     android:key="pluginCore"
+                    android:summary="@string/selectedValue"
                     android:title="@string/pluginCore_title" />
             </PreferenceScreen>
         </PreferenceCategory>

commit 25e8bede19cad5daa16cfc83940a6e4bbd358e20

bugfix: Fixed ListPreference summaries for pre-ICS devices.
Bug introduced in commit
59ca551bd27f1db09dca295c2e9f2c28b1da85bd

Android's documentation for ListPreference.getSummary is misleading, as
the string substitution feature was not introduced until API 14.
http://developer.android.com/reference/android/preference/ListPreference.html

src/paulscode/android/mupen64plusae/MenuActivity.java
@@ -225,6 +225,10 @@ public class MenuActivity extends PreferenceActivity implements OnPreferenceClic
         PrefUtil.enablePreference( this, TOUCHSCREEN_SIZE, user.isTouchscreenEnabled
                 && !user.isTouchscreenCustom );
         
+        // Update the summary text for all relevant preferences
+        for( String key : sharedPreferences.getAll().keySet() )
+            PrefUtil.refreshSummary( this, key );
+        
         // Update the summary text in a particular way for ACRA user info
         EditTextPreference pref = (EditTextPreference) findPreference( ACRA_USER_EMAIL );
         String value = pref.getText();

src/paulscode/android/mupen64plusae/persistent/AppData.java
@@ -91,6 +91,9 @@ public class AppData
     /** True if device is running Honeycomb MR1 or later (12 - Android 3.1.x) */
     public static final boolean IS_HONEYCOMB_MR1 = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1;
     
+    /** True if device is running Honeycomb MR1 or later (12 - Android 3.1.x) */
+    public static final boolean IS_ICE_CREAM_SANDWICH = Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+    
     /** True if device is running Jellybean or later (16 - Android 4.1.x) */
     public static final boolean IS_JELLYBEAN = Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
     

src/paulscode/android/mupen64plusae/util/PrefUtil.java
@@ -20,6 +20,8 @@
  */
 package paulscode.android.mupen64plusae.util;
 
+import paulscode.android.mupen64plusae.persistent.AppData;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
@@ -52,5 +54,16 @@ public class PrefUtil
         Preference child = activity.findPreference( keyChild );
         if( parent instanceof PreferenceGroup && child != null )
             ( (PreferenceGroup) parent ).removePreference( child );
+    }
+    
+    @SuppressWarnings( "deprecation" )
+    public static void refreshSummary( PreferenceActivity activity, String key )
+    {
+        if( !AppData.IS_ICE_CREAM_SANDWICH )
+        {
+            Preference preference = activity.findPreference( key );
+            if( preference instanceof ListPreference )
+                preference.setSummary( ( (ListPreference) preference ).getEntry() );
+        }
     }
 }