mupen64plus-ae/mupen64plus-ae

commit cd6281fc4c20591d7849921b6d12e42d98560335

Fix unstreched frame position (gles2rice)
The frame is now correctly positioned at the center of the screen when
using unstreched mode.

jni/gles2rice/src/OGLRender.cpp
@@ -1101,7 +1101,7 @@ void OGLRender::glViewportWrapper(GLint x, GLint y, GLsizei width, GLsizei heigh
         mflag=flag;
 //        if( flag )  glOrthof(0, windowSetting.uDisplayWidth, windowSetting.uDisplayHeight, 0, -1, 1);
         OPENGL_CHECK_ERRORS;
-        glViewport(x,y,width,height);
+        glViewport(windowSetting.xpos,windowSetting.ypos,windowSetting.width,windowSetting.height);
         OPENGL_CHECK_ERRORS;
     }
 }

jni/gles2rice/src/Video.cpp
@@ -371,8 +371,11 @@ static bool StartVideo(void)
           }
           // Calculate aspect ratio
           const float ratio = ( romPAL ? 9.0f/11.0f : 0.75f );
-          int videoWidth = videoInfo->current_w;
-          int videoHeight = videoInfo->current_h;
+          int screenWidth = videoInfo->current_w;
+          int screenHeight = videoInfo->current_h;
+ 
+          int videoWidth = screenWidth;
+          int videoHeight = screenHeight;
 
           stretchVideo = (bool) Android_JNI_GetScreenStretch();
           if( !stretchVideo )
@@ -387,38 +390,18 @@ static bool StartVideo(void)
           int x = ( videoInfo->current_w - videoWidth ) / 2;
           int y = ( videoInfo->current_h - videoHeight ) / 2;
 
-          // TODO: Where do we save x and y values, so the frame isn't at top-left corner??
-          // We have the following in windowSetting struct.
-          //float   fViWidth, fViHeight;
-          //unsigned short        uViWidth, uViHeight;
-          //unsigned short        uDisplayWidth, uDisplayHeight;
-          //BOOL    bDisplayFullscreen;
-          //BOOL    bVerticalSync;
-          //float   fMultX, fMultY;
-          //int     vpLeftW, vpTopW, vpRightW, vpBottomW, vpWidthW, vpHeightW;
-          //int     statusBarHeight, statusBarHeightToUse, toolbarHeight, toolbarHeightToUse;
-          //BOOL    screenSaverStatus;
-          //struct {
-              //uint32      left;
-              //uint32      top;
-              //uint32      right;
-              //uint32      bottom;
-              //uint32      width;
-              //uint32      height;
-              //bool        needToClip;
-          //} clipping;
-          //int     timer;
-          //float   fps;    // frame per second
-          //float   dps;    // dlist per second
-          //uint32  lastSecFrameCount;
-          //uint32  lastSecDlistCount;
-        ////
+        windowSetting.uDisplayWidth = screenWidth;
+        windowSetting.uDisplayHeight = screenHeight;
+        printf( "Screen dimensions: %i,%i\n", windowSetting.uDisplayWidth, windowSetting.uDisplayHeight );
 
+        windowSetting.width = videoWidth;
+        windowSetting.height = videoHeight;
+        windowSetting.xpos = x;
+        windowSetting.ypos = y;        
         ConfigSetDefaultInt( l_ConfigVideoGeneral, "ScreenWidth", videoWidth, "Width of output window or fullscreen width" );
         ConfigSetDefaultInt( l_ConfigVideoGeneral, "ScreenHeight", videoHeight, "Height of output window or fullscreen height" );
-        windowSetting.uDisplayWidth = videoWidth;
-        windowSetting.uDisplayHeight = videoHeight;
-        printf( "Screen dimensions: %i,%i\n", windowSetting.uDisplayWidth, windowSetting.uDisplayHeight );
+
+
 
 #endif
 

jni/gles2rice/src/Video.h
@@ -38,6 +38,7 @@ typedef struct {
     int     vpLeftW, vpTopW, vpRightW, vpBottomW, vpWidthW, vpHeightW;
 
     int     statusBarHeight, statusBarHeightToUse, toolbarHeight, toolbarHeightToUse;
+    int     xpos, ypos, width, height;
     BOOL    screenSaverStatus;
 
     struct {

commit d6a35b0fb1ec4933c788f948c71cc3f3dcfc9fb5

gles2rice: fix bugs introduced in commit cd6281f
This is fixing all known bugs (mario kart in multiplayer, zelda blue
action icon and surely others) introduced in this commit:
https://github.com/paulscode/mupen64plus-ae/commit/cd6281f

jni/gles2rice/src/OGLRender.cpp
@@ -115,7 +115,7 @@ static int hardwareType = HARDWARE_TYPE_UNKNOWN;
 ////
 void OGLRender::Initialize(void)
 {
-    glViewportWrapper(0, windowSetting.statusBarHeightToUse, windowSetting.uDisplayWidth, windowSetting.uDisplayHeight);
+    glViewportWrapper(windowSetting.xpos, windowSetting.ypos, windowSetting.uDisplayWidth, windowSetting.uDisplayHeight);
     OPENGL_CHECK_ERRORS;
 
     COGLGraphicsContext *pcontext = (COGLGraphicsContext *)(CGraphicsContext::g_pGraphicsContext);
@@ -519,7 +519,7 @@ void OGLRender::SetTextureVFlag(TextureUVFlag dwFlag, uint32 dwTile)
 
 bool OGLRender::RenderTexRect()
 {
-    glViewportWrapper(0, windowSetting.statusBarHeightToUse, windowSetting.uDisplayWidth, windowSetting.uDisplayHeight);
+    glViewportWrapper(windowSetting.xpos, windowSetting.ypos, windowSetting.uDisplayWidth, windowSetting.uDisplayHeight);
     OPENGL_CHECK_ERRORS;
 
     GLboolean cullface = glIsEnabled(GL_CULL_FACE);
@@ -578,7 +578,7 @@ bool OGLRender::RenderFillRect(uint32 dwColor, float depth)
     float r = ((dwColor>>16)&0xFF)/255.0f;
     float g = ((dwColor>>8)&0xFF)/255.0f;
     float b = (dwColor&0xFF)/255.0f;
-    glViewportWrapper(0, windowSetting.statusBarHeightToUse, windowSetting.uDisplayWidth, windowSetting.uDisplayHeight);
+    glViewportWrapper(windowSetting.xpos, windowSetting.ypos, windowSetting.uDisplayWidth, windowSetting.uDisplayHeight);
     OPENGL_CHECK_ERRORS;
 
     GLboolean cullface = glIsEnabled(GL_CULL_FACE);
@@ -660,7 +660,7 @@ bool OGLRender::RenderFlushTris()
 
     ApplyZBias(m_dwZBias);                    // set the bias factors
 
-    glViewportWrapper(windowSetting.vpLeftW, windowSetting.uDisplayHeight-windowSetting.vpTopW-windowSetting.vpHeightW+windowSetting.statusBarHeightToUse, windowSetting.vpWidthW, windowSetting.vpHeightW, false);
+    glViewportWrapper(windowSetting.vpLeftW + windowSetting.xpos, windowSetting.uDisplayHeight-windowSetting.vpTopW-windowSetting.vpHeightW+windowSetting.ypos, windowSetting.vpWidthW, windowSetting.vpHeightW, false);
     OPENGL_CHECK_ERRORS;
 
 
@@ -729,7 +729,7 @@ void OGLRender::DrawSimple2DTexture(float x0, float y0, float x1, float y1, floa
     glDisable(GL_CULL_FACE);
     OPENGL_CHECK_ERRORS;
 
-    glViewportWrapper(0, windowSetting.statusBarHeightToUse, windowSetting.uDisplayWidth, windowSetting.uDisplayHeight);
+    glViewportWrapper(windowSetting.xpos, windowSetting.ypos, windowSetting.uDisplayWidth, windowSetting.uDisplayHeight);
     OPENGL_CHECK_ERRORS;
 
     float a = (g_texRectTVtx[0].dcDiffuse >>24)/255.0f;
@@ -864,7 +864,7 @@ COLOR OGLRender::PostProcessSpecularColor()
 
 void OGLRender::SetViewportRender()
 {
-    glViewportWrapper(windowSetting.vpLeftW, windowSetting.uDisplayHeight-windowSetting.vpTopW-windowSetting.vpHeightW+windowSetting.statusBarHeightToUse, windowSetting.vpWidthW, windowSetting.vpHeightW);
+    glViewportWrapper(windowSetting.vpLeftW + windowSetting.xpos, windowSetting.uDisplayHeight-windowSetting.vpTopW-windowSetting.vpHeightW+windowSetting.ypos, windowSetting.vpWidthW, windowSetting.vpHeightW);
     OPENGL_CHECK_ERRORS;
 }
 
@@ -952,7 +952,7 @@ void OGLRender::UpdateScissor()
         uint32 height = (gRDP.scissor.right*gRDP.scissor.bottom)/width;
         glEnable(GL_SCISSOR_TEST);
         OPENGL_CHECK_ERRORS;
-        glScissor(0, int(height*windowSetting.fMultY+windowSetting.statusBarHeightToUse),
+        glScissor(windowSetting.xpos, int(height*windowSetting.fMultY+windowSetting.ypos),
             int(width*windowSetting.fMultX), int(height*windowSetting.fMultY) );
         OPENGL_CHECK_ERRORS;
     }
@@ -973,13 +973,13 @@ void OGLRender::ApplyRDPScissor(bool force)
         uint32 height = (gRDP.scissor.right*gRDP.scissor.bottom)/width;
         glEnable(GL_SCISSOR_TEST);
         OPENGL_CHECK_ERRORS;
-        glScissor(0, int(height*windowSetting.fMultY+windowSetting.statusBarHeightToUse),
+        glScissor(windowSetting.xpos, int(height*windowSetting.fMultY+windowSetting.ypos),
             int(width*windowSetting.fMultX), int(height*windowSetting.fMultY) );
         OPENGL_CHECK_ERRORS;
     }
     else
     {
-        glScissor(int(gRDP.scissor.left*windowSetting.fMultX), int((windowSetting.uViHeight-gRDP.scissor.bottom)*windowSetting.fMultY+windowSetting.statusBarHeightToUse),
+        glScissor(int((gRDP.scissor.left*windowSetting.fMultX)+windowSetting.xpos), int((windowSetting.uViHeight-gRDP.scissor.bottom)*windowSetting.fMultY+windowSetting.ypos),
             int((gRDP.scissor.right-gRDP.scissor.left)*windowSetting.fMultX), int((gRDP.scissor.bottom-gRDP.scissor.top)*windowSetting.fMultY ));
         OPENGL_CHECK_ERRORS;
     }
@@ -993,7 +993,7 @@ void OGLRender::ApplyScissorWithClipRatio(bool force)
 
     glEnable(GL_SCISSOR_TEST);
     OPENGL_CHECK_ERRORS;
-    glScissor(windowSetting.clipping.left, int((windowSetting.uViHeight-gRSP.real_clip_scissor_bottom)*windowSetting.fMultY)+windowSetting.statusBarHeightToUse,
+    glScissor(int(windowSetting.clipping.left + windowSetting.xpos), int((windowSetting.uViHeight-gRSP.real_clip_scissor_bottom)*windowSetting.fMultY)+windowSetting.ypos,
         windowSetting.clipping.width, windowSetting.clipping.height);
     OPENGL_CHECK_ERRORS;
 
@@ -1101,7 +1101,7 @@ void OGLRender::glViewportWrapper(GLint x, GLint y, GLsizei width, GLsizei heigh
         mflag=flag;
 //        if( flag )  glOrthof(0, windowSetting.uDisplayWidth, windowSetting.uDisplayHeight, 0, -1, 1);
         OPENGL_CHECK_ERRORS;
-        glViewport(windowSetting.xpos,windowSetting.ypos,windowSetting.width,windowSetting.height);
+        glViewport(x,y,width,height);
         OPENGL_CHECK_ERRORS;
     }
 }

jni/gles2rice/src/OGLRenderExt.cpp
@@ -32,7 +32,7 @@ void OGLRender::DrawText(const char* str, RECT *rect)
 
 void OGLRender::DrawSpriteR_Render()    // With Rotation
 {
-    glViewportWrapper(0, windowSetting.statusBarHeightToUse, windowSetting.uDisplayWidth, windowSetting.uDisplayHeight);
+    glViewportWrapper(windowSetting.xpos, windowSetting.ypos, windowSetting.uDisplayWidth, windowSetting.uDisplayHeight);
 
     GLboolean cullface = glIsEnabled(GL_CULL_FACE);
     glDisable(GL_CULL_FACE);

jni/gles2rice/src/Video.cpp
@@ -377,11 +377,8 @@ static bool StartVideo(void)
           }
           // Calculate aspect ratio
           const float ratio = ( romPAL ? 9.0f/11.0f : 0.75f );
-          int screenWidth = videoInfo->current_w;
-          int screenHeight = videoInfo->current_h;
- 
-          int videoWidth = screenWidth;
-          int videoHeight = screenHeight;
+          int videoWidth = videoInfo->current_w;
+          int videoHeight = videoInfo->current_h;
           int screenPosition;
           int x, y;
 
@@ -420,19 +417,14 @@ static bool StartVideo(void)
               y = ( videoInfo->current_h - videoHeight ) / 2;
           }
 
-        windowSetting.uDisplayWidth = screenWidth;
-        windowSetting.uDisplayHeight = screenHeight;
-        printf( "Screen dimensions: %i,%i\n", windowSetting.uDisplayWidth, windowSetting.uDisplayHeight );
-
-        windowSetting.width = videoWidth;
-        windowSetting.height = videoHeight;
+        windowSetting.uDisplayWidth = videoWidth;
+        windowSetting.uDisplayHeight = videoHeight;
         windowSetting.xpos = x;
-        windowSetting.ypos = y;        
+        windowSetting.ypos = y;   
+        
+        printf( "Screen dimensions: %i,%i\n", windowSetting.uDisplayWidth, windowSetting.uDisplayHeight );     
         ConfigSetDefaultInt( l_ConfigVideoGeneral, "ScreenWidth", videoWidth, "Width of output window or fullscreen width" );
         ConfigSetDefaultInt( l_ConfigVideoGeneral, "ScreenHeight", videoHeight, "Height of output window or fullscreen height" );
-
-
-
 #endif
 
         bool res = CGraphicsContext::Get()->Initialize( windowSetting.uDisplayWidth, windowSetting.uDisplayHeight,

jni/gles2rice/src/Video.h
@@ -38,7 +38,7 @@ typedef struct {
     int     vpLeftW, vpTopW, vpRightW, vpBottomW, vpWidthW, vpHeightW;
 
     int     statusBarHeight, statusBarHeightToUse, toolbarHeight, toolbarHeightToUse;
-    int     xpos, ypos, width, height;
+    int     xpos, ypos;
     BOOL    screenSaverStatus;
 
     struct {