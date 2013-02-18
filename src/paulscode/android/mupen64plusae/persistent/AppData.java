4588c024565016173faf3281ebfdbcefe49401e7
littleguy77 <mattlichter@live.com>
2013-02-17 23:17:37 -0500

    front: Added Galaxy S2 4G to flicker-reduction profile.

--- src/paulscode/android/mupen64plusae/persistent/AppData.java
+++ src/paulscode/android/mupen64plusae/persistent/AppData.java
@@ -439,6 +439,7 @@ public class AppData
                     || hardware.contains( "mapphone" )
                     || hardware.contains( "amlogic meson3" )
                     || hardware.contains( "rk30board" )
+                    || hardware.contains( "smdk4210" )
                     || hardware.contains( "cardhu" ) )
                 hardwareType = HARDWARE_TYPE_OMAP_2;
             
