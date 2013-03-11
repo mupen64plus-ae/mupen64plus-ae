61fedec844da2ab1df0371c6a672c9b95cc26474
littleguy77 <mattlichter@live.com>
2013-03-11 15:56:06 -0400

    gles2rice: Use SDL preprocessor symbols to target correct GL variant.
    Apparently, this should auto-magically detect the appropriate OpenGL
    variant to use for the platform. See <SDL_config.h>.  In our case SDL
    keys off of the ANDROID symbol to determine that gles2 should be used.
    
    We should probably use the SDL_VIDEO_OPENGL* family of symbols for
    targeting code to different GL platforms.
    
    TODO: Test with other platforms to be sure the OpenGL compiler paths
    are used appropriately.

--- jni/gles2rice/src/OGLPlatform.h
+++ jni/gles2rice/src/OGLPlatform.h
@@ -1,8 +1,11 @@
-#ifdef GLES_2
-#include "GLES2/gl2.h"
-#include "GLES2/gl2ext.h"
-#define GLSL_VERSION "100"
-#else
-#include <GL\glew.h>
+#include <SDL_config.h>
+
+#if SDL_VIDEO_OPENGL
+#include <SDL_opengl.h>
 #define GLSL_VERSION "120"
+
+#elif SDL_VIDEO_OPENGL_ES2
+#include <SDL_opengles2.h>
+#define GLSL_VERSION "100"
+
 #endif
