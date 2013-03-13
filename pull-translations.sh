e48836941e80bef449795c6916ac29fc2518d921
Sven Eckelmann <sven@narfation.org>
2013-03-13 16:07:18 +0100

    res: Fix shell syntax for transifex pull script

--- pull-translations.sh
+++ pull-translations.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 # Pull the transifex translation files using the command-line tool.
 
 pattern='./res/values-*/strings.xml ./doc/publish/listing-*.txt'
@@ -6,7 +8,7 @@ git pull
 tx pull -a -f
 
 environment=`uname -o`
-if [ "$environment" == "Cygwin" -o "$environment" == "Msys" ]; then
+if [ "$environment" = "Cygwin" -o "$environment" = "Msys" ]; then
     echo "Converting line endings to CRLF"
     unix2dos $pattern
 else
