16673849f7561f3299809d2bbfcc3e313ee5b01c
Lioncash <mathew1800@gmail.com>
2013-03-14 08:15:55 +0100

    Add missing NO_ASM block for texture crc calculation

--- src/FrameBuffer.cpp
+++ src/FrameBuffer.cpp
@@ -744,6 +744,31 @@ l1:             mov esi, [ecx+ebx]
                 );
            dwAsmCRC = asmCRC;
 # endif // defined(__PIC__)
+#else
+            uint32 pitch = pitchInBytes>>2;
+            uint32* pStart = (uint32*)pPhysicalAddress;
+            pStart += (top * pitch) + (((left<<size)+1)>>3);
+
+            uint32 y = dwAsmHeight;
+            uint32 x, esi = 0;
+
+            while((int)y >= 0)
+            {
+                x = dwAsmdwBytesPerLine - 4;
+                while((int)x >= 0)
+                {
+                    esi = *(uint32*)(pAsmStart + x);
+                    esi ^= x;
+
+                    dwAsmCRC = (dwAsmCRC << 4) + ((dwAsmCRC >> 28) & 15);
+                    dwAsmCRC += esi;
+                    x-=4;
+                }
+                esi ^= y;
+                dwAsmCRC += esi;
+                pAsmStart += dwAsmPitch;
+                y--;
+            }
 #endif
         }
         catch(...)
