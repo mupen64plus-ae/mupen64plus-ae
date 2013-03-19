2849db3ad60492974e6ea111f144c1726468fff2
littleguy77 <mattlichter@live.com>
2013-03-19 19:11:35 -0400

    acra: Add library (.so) names to ACRA report.
    By far the largest single cause of crashes right now is due to link
    errors with the libraries at run-time (34% of all reported crashes).
    
    https://paulscode.iriscouch.com/acralyzer-custom/_design/acralyzer/index.html#/dashboard/mupen64plusae
    
    This just adds the names of whatever files are found in the libraries
    directory. Just want to rule out the possibility that the files weren't
    installed properly.

--- src/paulscode/android/mupen64plusae/persistent/AppData.java
+++ src/paulscode/android/mupen64plusae/persistent/AppData.java
@@ -21,18 +21,21 @@
 package paulscode.android.mupen64plusae.persistent;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Locale;
 
 import org.acra.ACRA;
 import org.acra.ErrorReporter;
 
 import paulscode.android.mupen64plusae.util.DeviceUtil;
+import paulscode.android.mupen64plusae.util.FileUtil;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Build;
 import android.os.Environment;
+import android.text.TextUtils;
 import android.util.Log;
 
 /**
@@ -209,8 +212,15 @@ public class AppData
         String appDataFilename = packageName + "_appdata";
         mPreferences = context.getSharedPreferences( appDataFilename, Context.MODE_PRIVATE );
         
+        // Get the contents of the libraries directory
+        ArrayList<CharSequence> names = new ArrayList<CharSequence>();
+        ArrayList<String> paths = new ArrayList<String>();
+        FileUtil.populate( new File( libsDir ), false, false, true, names, paths );
+        String libnames = TextUtils.join( "\n", names );
+
         // Record some info in the crash reporter
         ErrorReporter reporter = ACRA.getErrorReporter();
+        reportMultilineText( reporter, "Libraries", libnames );
         reporter.putCustomData( "CPU Features", hardwareInfo.features );
         reporter.putCustomData( "CPU Hardware", hardwareInfo.hardware );
         reporter.putCustomData( "CPU Processor", hardwareInfo.processor );
