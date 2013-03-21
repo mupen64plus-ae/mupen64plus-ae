b4bb6a5dee47738346aec4b8e11af3120ccfe035
littleguy77 <mattlichter@live.com>
2013-03-20 22:55:29 -0400

    build: Fool-proofed the rsp-hle make file.
    Everything is defined exactly once.  Simplifies maintenance, reduces
    likelihood of errors.

--- jni/rsp-hle/projects/android/Android.mk
+++ jni/rsp-hle/projects/android/Android.mk
@@ -3,14 +3,14 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 SRCDIR := ../../src
 
-LOCAL_MODULE := rsp-hle
-LOCAL_SHARED_LIBRARIES := core
-LOCAL_STATIC_LIBRARIES := cpufeatures
-LOCAL_ARM_MODE := arm
+MY_LOCAL_MODULE := rsp-hle
+MY_LOCAL_SHARED_LIBRARIES := core
+MY_LOCAL_STATIC_LIBRARIES := cpufeatures
+MY_LOCAL_ARM_MODE := arm
 
-LOCAL_C_INCLUDES := $(M64P_API_INCLUDES)
+MY_LOCAL_C_INCLUDES := $(M64P_API_INCLUDES)
 
-LOCAL_SRC_FILES :=          \
+MY_LOCAL_SRC_FILES :=       \
 	$(SRCDIR)/alist.c       \
 	$(SRCDIR)/cicx105.c     \
 	$(SRCDIR)/jpeg.c        \
@@ -20,52 +20,52 @@ LOCAL_SRC_FILES :=          \
 	$(SRCDIR)/ucode3.cpp    \
 	$(SRCDIR)/ucode3mp3.cpp \
 
-LOCAL_CFLAGS :=         \
+MY_LOCAL_CFLAGS :=      \
     $(COMMON_CFLAGS)    \
     -DNO_ASM            \
 
-LOCAL_CPPFLAGS := $(COMMON_CPPFLAGS)
+MY_LOCAL_CPPFLAGS := $(COMMON_CPPFLAGS)
 
-LOCAL_LDFLAGS := -Wl,-version-script,$(LOCAL_PATH)/$(SRCDIR)/rsp_api_export.ver
+MY_LOCAL_LDFLAGS := -Wl,-version-script,$(LOCAL_PATH)/$(SRCDIR)/rsp_api_export.ver
 
-LOCAL_LDLIBS := $(COMMON_LDLIBS)
+MY_LOCAL_LDLIBS := $(COMMON_LDLIBS)
+
+######### Standard RSP Module #################################################
+
+include $(CLEAR_VARS)
+
+LOCAL_SHARED_LIBRARIES  := $(MY_LOCAL_SHARED_LIBRARIES)
+LOCAL_STATIC_LIBRARIES  := $(MY_LOCAL_STATIC_LIBRARIES)
+LOCAL_ARM_MODE          := $(MY_LOCAL_ARM_MODE)
+LOCAL_C_INCLUDES        := $(MY_LOCAL_C_INCLUDES)
+LOCAL_SRC_FILES         := $(MY_LOCAL_SRC_FILES)
+LOCAL_CFLAGS            := $(MY_LOCAL_CFLAGS)
+LOCAL_CPPFLAGS          := $(MY_LOCAL_CPPFLAGS)
+LOCAL_LDFLAGS           := $(MY_LOCAL_LDFLAGS)
+LOCAL_LDLIBS            := $(MY_LOCAL_LDLIBS)
+
+LOCAL_MODULE := $(MY_LOCAL_MODULE)
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module, android/cpufeatures)
 
-###############################################################################
+######### Same Module, No Sound ###############################################
 
 include $(CLEAR_VARS)
-SRCDIR := ../../src
 
-LOCAL_MODULE := rsp-hle-nosound
-LOCAL_SHARED_LIBRARIES := core
-LOCAL_STATIC_LIBRARIES := cpufeatures
-LOCAL_ARM_MODE := arm
+LOCAL_SHARED_LIBRARIES  := $(MY_LOCAL_SHARED_LIBRARIES)
+LOCAL_STATIC_LIBRARIES  := $(MY_LOCAL_STATIC_LIBRARIES)
+LOCAL_ARM_MODE          := $(MY_LOCAL_ARM_MODE)
+LOCAL_C_INCLUDES        := $(MY_LOCAL_C_INCLUDES)
+LOCAL_SRC_FILES         := $(MY_LOCAL_SRC_FILES)
+LOCAL_CFLAGS            := $(MY_LOCAL_CFLAGS)
+LOCAL_CPPFLAGS          := $(MY_LOCAL_CPPFLAGS)
+LOCAL_LDFLAGS           := $(MY_LOCAL_LDFLAGS)
+LOCAL_LDLIBS            := $(MY_LOCAL_LDLIBS)
 
-LOCAL_C_INCLUDES := $(M64P_API_INCLUDES)
-
-LOCAL_SRC_FILES :=          \
-	$(SRCDIR)/alist.c       \
-	$(SRCDIR)/cicx105.c     \
-	$(SRCDIR)/jpeg.c        \
-	$(SRCDIR)/main.c        \
-	$(SRCDIR)/ucode1.cpp    \
-	$(SRCDIR)/ucode2.cpp    \
-	$(SRCDIR)/ucode3.cpp    \
-	$(SRCDIR)/ucode3mp3.cpp \
-
-LOCAL_CFLAGS :=         \
-    $(COMMON_CFLAGS)    \
-    -DNO_ASM            \
-
-LOCAL_CPPFLAGS := $(COMMON_CPPFLAGS)
-
-LOCAL_LDFLAGS := -Wl,-version-script,$(LOCAL_PATH)/$(SRCDIR)/rsp_api_export.ver
-
-LOCAL_LDLIBS := $(COMMON_LDLIBS)
-
+# Only difference is the module name and a flag
+LOCAL_MODULE := $(MY_LOCAL_MODULE)-nosound
 LOCAL_CFLAGS += -DM64P_NO_AUDIO
 
 include $(BUILD_SHARED_LIBRARY)
