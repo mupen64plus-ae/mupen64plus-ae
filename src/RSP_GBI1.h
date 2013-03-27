ba0350a9998e1842c65f0fd3d637bbc1c6412179
Richard Goedeken <Richard@fascinationsoftware.com>
2013-03-26 22:30:23 -0700

    fix apparent vertex index calculation bug found by Lioncash

--- src/RSP_GBI1.h
+++ src/RSP_GBI1.h
@@ -163,7 +163,7 @@ void RSP_GBI1_BranchZ(Gfx *gfx)
 {
     SP_Timing(RSP_GBI1_BranchZ);
 
-    uint32 vtx = ((gfx->words.w0)&0xFFF)>1;
+    uint32 vtx = ((gfx->words.w0)&0xFFF)>>1;
     float vtxdepth = g_vecProjected[vtx].z/g_vecProjected[vtx].w;
 
 #ifdef DEBUGGER
