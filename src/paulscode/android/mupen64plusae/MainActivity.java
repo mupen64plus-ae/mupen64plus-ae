25ccfedefdc5e68897fafd4fbe9a93351069943f
paulscode <paul@paulscode.com>
2013-04-24 17:01:25 -0500

    cheats: Added cheat to fix Pokemon Snap camera bug
    This isn't the best solution to the problem, but it does make the
    game playable.

--- src/paulscode/android/mupen64plusae/MainActivity.java
+++ src/paulscode/android/mupen64plusae/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends Activity implements OnExtractionProgressListen
      * Asset version number, used to determine stale assets. Increment this number every time the
      * assets are updated on disk.
      */
-    private static final int ASSET_VERSION = 12;
+    private static final int ASSET_VERSION = 13;
     
     /** The total number of assets to be extracted (for computing progress %). */
     private static final int TOTAL_ASSETS = 120;
