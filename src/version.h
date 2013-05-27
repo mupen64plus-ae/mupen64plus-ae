f423bd239f96ce551339febcfa886f36b38297b7
Richard Goedeken <Richard@fascinationsoftware.com>
2013-05-26 22:21:52 -0700

    1. Update Core Video Extension API to v3.0.0, pass flag to VidExt_SetVideoMode() that we support resizing 2. New ResizeInitialize method in COGLGraphicsContext to do a partial re-initialization after OpenGL context is recreated following window resize 3. Add function (ResizeStep2) to asynchronously handle resized window 4. Update Video API to v2.2.0: add function ResizeVideoOutput()

--- src/version.h
+++ src/version.h
@@ -28,9 +28,9 @@
 
 #define PLUGIN_NAME    "Mupen64Plus OpenGL Video Plugin by Rice"
 #define PLUGIN_VERSION           0x016305
-#define VIDEO_PLUGIN_API_VERSION 0x020100
+#define VIDEO_PLUGIN_API_VERSION 0x020200
 #define CONFIG_API_VERSION       0x020000
-#define VIDEXT_API_VERSION       0x020000
+#define VIDEXT_API_VERSION       0x030000
 
 #define VERSION_PRINTF_SPLIT(x) (((x) >> 16) & 0xffff), (((x) >> 8) & 0xff), ((x) & 0xff)
 
