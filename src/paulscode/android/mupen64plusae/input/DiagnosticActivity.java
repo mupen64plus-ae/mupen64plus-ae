e884b39a2fc1116a107590781e75148e1323b60d
littleguy77 <mattlichter@live.com>
2013-06-06 21:01:12 -0400

    front: Minor cleanup of DiagnosticActivity.
    Changed some function names for clarity, shuffled some lines of code.
    Getting the abstracted hardware id instead of what the O/S gives us
    (currently equivalent, but possibly not in the future).

--- src/paulscode/android/mupen64plusae/input/DiagnosticActivity.java
+++ src/paulscode/android/mupen64plusae/input/DiagnosticActivity.java
@@ -29,7 +29,6 @@ import paulscode.android.mupen64plusae.persistent.UserPrefs;
 import paulscode.android.mupen64plusae.util.DeviceUtil;
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.InputDevice.MotionRange;
 import android.view.KeyEvent;
@@ -49,62 +48,60 @@ public class DiagnosticActivity extends Activity
     @Override
     public boolean onKeyDown( int keyCode, KeyEvent event )
     {
-        return onKey( event );
+        onKey( event );
+        if( keyCode == KeyEvent.KEYCODE_BACK )
+            return super.onKeyDown( keyCode, event );
+        return true;
     }
     
     @Override
     public boolean onKeyUp( int keyCode, KeyEvent event )
     {
-        return onKey( event );
+        onKey( event );
+        if( keyCode == KeyEvent.KEYCODE_BACK )
+            return super.onKeyUp( keyCode, event );
+        return true;
+    }
+    
+    @TargetApi( 12 )
+    private void onKey( KeyEvent event )
+    {
+        int keyCode = event.getKeyCode();
+        
+        String message = "KeyEvent:";
+        message += "\nDevice: " + getHardwareSummary( AbstractProvider.getHardwareId( event ) );
+        message += "\nAction: " + DeviceUtil.getActionName( event.getAction(), false );
+        message += "\nKeyCode: " + keyCode;
+        
+        if( AppData.IS_HONEYCOMB_MR1 )
+        {
+            message += "\n\n" + KeyEvent.keyCodeToString( keyCode );
+        }
+        
+        TextView view = (TextView) findViewById( R.id.textKey );
+        view.setText( message );
     }
     
     @Override
     public boolean onTouchEvent( MotionEvent event )
     {
-        return onMotion( event );
+        onMotion( event );
+        return true;
     }
     
     @TargetApi( 12 )
     @Override
     public boolean onGenericMotionEvent( MotionEvent event )
     {
-        return onMotion( event );
-    }
-    
-    @TargetApi( Build.VERSION_CODES.HONEYCOMB_MR1 )
-    private boolean onKey( KeyEvent event )
-    {
-        int key = event.getKeyCode();
-        
-        String message = "KeyEvent:";
-        message += "\nDevice: " + getDeviceName( event.getDeviceId() );
-        message += "\nAction: " + DeviceUtil.getActionName( event.getAction(), false );
-        message += "\nKeyCode: " + key;
-        
-        if( AppData.IS_HONEYCOMB_MR1 )
-        {
-            message += "\n\n" + KeyEvent.keyCodeToString( key );
-        }
-        
-        TextView view = (TextView) findViewById( R.id.textKey );
-        view.setText( message );
-        
-        if( key == KeyEvent.KEYCODE_BACK )
-        {
-            if( event.getAction() == 0 )
-                return super.onKeyDown( key, event );
-            else
-                return super.onKeyUp( key, event );
-        }
-        else
-            return true;
+        onMotion( event );
+        return true;
     }
     
     @TargetApi( 12 )
-    private boolean onMotion( MotionEvent event )
+    private void onMotion( MotionEvent event )
     {
         String message = "MotionEvent:";
-        message += "\nDevice: " + getDeviceName( event.getDeviceId() );
+        message += "\nDevice: " + getHardwareSummary( AbstractProvider.getHardwareId( event ) );
         message += "\nAction: " + DeviceUtil.getActionName( event.getAction(), true );
         message += "\n";
         
@@ -130,12 +127,11 @@ public class DiagnosticActivity extends Activity
         
         TextView view = (TextView) findViewById( R.id.textMotion );
         view.setText( message );
-        return true;
     }
     
-    private static String getDeviceName( int device )
+    private static String getHardwareSummary( int hardwareId )
     {
-        String name = AbstractProvider.getHardwareName( device );
-        return Integer.toString( device ) + ( name == null ? "" : " (" + name + ")" );
+        String name = AbstractProvider.getHardwareName( hardwareId );
+        return Integer.toString( hardwareId ) + ( name == null ? "" : " (" + name + ")" );
     }
 }
