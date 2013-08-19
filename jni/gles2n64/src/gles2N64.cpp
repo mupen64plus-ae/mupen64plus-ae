mupen64plus-ae/mupen64plus-ae

commit ae8db02bbe96d842809f8e007557da3654a08496

gles2n64: Replaced frameskip JNI functions with config file parameters.
These values are static throughout an emulation session, so no need for
run-time communication via the JNI. Just pass the data via config file.

jni/gles2n64/src/gles2N64.cpp
@@ -46,15 +46,6 @@ EXPORT m64p_error CALL PluginStartup(m64p_dynlib_handle CoreLibHandle,
         gSPInitNeon();
     }
 #endif
-    ticksInitialize();
-
-    int max_frames = Android_JNI_GetMaxFrameSkip();
-// TODO: get rid of this, it should be handled through the config file:
-    if( Android_JNI_GetAutoFrameSkip() )
-        frameSkipper.setSkips( FrameSkipper::AUTO, max_frames );
-    else
-        frameSkipper.setSkips( FrameSkipper::MANUAL, max_frames );
-//
     return M64ERR_SUCCESS;
 }
 
@@ -131,6 +122,12 @@ EXPORT int CALL InitiateGFX (GFX_INFO Gfx_Info)
 
     Config_LoadConfig();
     Config_LoadRomConfig(Gfx_Info.HEADER);
+
+    ticksInitialize();
+    if( config.autoFrameSkip )
+        frameSkipper.setSkips( FrameSkipper::AUTO, config.maxFrameSkip );
+    else
+        frameSkipper.setSkips( FrameSkipper::MANUAL, config.maxFrameSkip );
 
     OGL_Start();
 

jni/ae-bridge/ae_bridge.h
@@ -64,8 +64,6 @@ extern void         Android_JNI_State_Callback(int paramChanged, int newValue);
 
 // Native utilities - TODO: Move to configuration file if possible
 extern int          Android_JNI_GetHardwareType();
-extern int          Android_JNI_GetAutoFrameSkip();
-extern int          Android_JNI_GetMaxFrameSkip();
 extern int          Android_JNI_UseRGBA8888();
 extern void         Android_JNI_GetPolygonOffset(const int hardwareType, const int bias, float* f1, float* f2);
 

jni/ae-bridge/ae_imports.cpp
@@ -42,8 +42,6 @@ static jclass mActivityClass;
 // Imported java method references
 static jmethodID midStateCallback;
 static jmethodID midGetHardwareType;
-static jmethodID midGetAutoFrameSkip;
-static jmethodID midGetMaxFrameSkip;
 static jmethodID midUseRGBA8888;
 
 /*******************************************************************************
@@ -159,11 +157,9 @@ extern DECLSPEC void SDL_Android_Init_Extras(JNIEnv* env, jclass cls)
 
     midStateCallback        = env->GetStaticMethodID(mActivityClass, "stateCallback",      "(II)V");
     midGetHardwareType      = env->GetStaticMethodID(mActivityClass, "getHardwareType",    "()I");
-    midGetAutoFrameSkip     = env->GetStaticMethodID(mActivityClass, "getAutoFrameSkip",   "()Z");
-    midGetMaxFrameSkip      = env->GetStaticMethodID(mActivityClass, "getMaxFrameSkip",    "()I");
     midUseRGBA8888          = env->GetStaticMethodID(mActivityClass, "useRGBA8888",        "()Z");
 
-    if (!midStateCallback || !midGetHardwareType || !midGetAutoFrameSkip || !midGetMaxFrameSkip || !midUseRGBA8888)
+    if (!midStateCallback || !midGetHardwareType || !midUseRGBA8888)
     {
         LOGE("Couldn't locate Java callbacks, check that they're named and typed correctly");
     }
@@ -182,16 +178,6 @@ extern DECLSPEC void Android_JNI_State_Callback(int paramChanged, int newValue)
 extern DECLSPEC int Android_JNI_GetHardwareType()
 {
     return GetInt(midGetHardwareType);
-}
-
-extern DECLSPEC int Android_JNI_GetAutoFrameSkip()
-{
-    return GetBooleanAsInt(midGetAutoFrameSkip);
-}
-
-extern DECLSPEC int Android_JNI_GetMaxFrameSkip()
-{
-    return GetInt(midGetMaxFrameSkip);
 }
 
 extern DECLSPEC int Android_JNI_UseRGBA8888()

jni/gles2n64/src/Config.cpp
@@ -99,6 +99,7 @@ Option configOptions[] =
 
     {"#Frame skip:", NULL, 0},
     {"auto frameskip", &config.autoFrameSkip, 0},
+    {"max frameskip", &config.maxFrameSkip, 0},
     {"target FPS", &config.targetFPS, 20},
     {"frame render rate", &config.frameRenderRate, 1},
     {"vertical sync", &config.verticalSync, 0},

jni/gles2n64/src/Config.h
@@ -44,6 +44,7 @@ struct Config
     int     zHack;
 
     int     autoFrameSkip;
+    int     maxFrameSkip;
     int     targetFPS;
     int     frameRenderRate;
     int     verticalSync;

src/paulscode/android/mupen64plusae/CoreInterface.java
@@ -424,6 +424,8 @@ public class CoreInterface
         ConfigFile gles2n64_conf = new ConfigFile( appData.gles2n64_conf );
         gles2n64_conf.put( "[<sectionless!>]", "window width", Integer.toString( user.videoRenderWidth ) );
         gles2n64_conf.put( "[<sectionless!>]", "window height", Integer.toString( user.videoRenderHeight ) );
+        gles2n64_conf.put( "[<sectionless!>]", "auto frameskip", booleanToString( user.isGles2N64AutoFrameskipEnabled ) );
+        gles2n64_conf.put( "[<sectionless!>]", "max frameskip", Integer.toString( user.gles2N64MaxFrameskip ) );
         gles2n64_conf.put( "[<sectionless!>]", "enable fog", booleanToString( user.isGles2N64FogEnabled ) );
         gles2n64_conf.put( "[<sectionless!>]", "enable alpha test", booleanToString( user.isGles2N64AlphaTestEnabled ) );
         gles2n64_conf.put( "[<sectionless!>]", "force screen clear", booleanToString( user.isGles2N64ScreenClearEnabled ) );

src/paulscode/android/mupen64plusae/CoreInterfaceNative.java
@@ -209,26 +209,6 @@ public class CoreInterfaceNative extends CoreInterface
     }
     
     /**
-     * Checks if the GLES2N64 plugin is using auto-frameskip.
-     * 
-     * @return True if auto-framskip is enabled. False otherwise.
-     */
-    public static boolean getAutoFrameSkip()
-    {
-        return sUserPrefs.isGles2N64AutoFrameskipEnabled;
-    }
-    
-    /**
-     * Gets the maximum frameskips allowed for the GLES2N64 plugin.
-     * 
-     * @return The number of max frameskps allowed.
-     */
-    public static int getMaxFrameSkip()
-    {
-        return sUserPrefs.gles2N64MaxFrameskip;
-    }
-    
-    /**
      * Checks if the emulator is using RGBA 8888.
      * 
      * @return True if RGBA 8888 is being used. False otherwise.

commit 0d0b7f8e9e2e69e670a38a16e4b9bf483b0a4b39

gles2n64: Fixed frameskip-config regression.
This commit fixes Issue #272.

Auto-frameskip regressed in commit
ae8db02bbe96d842809f8e007557da3654a08496

Basically, that commit made gles2n64 use an obsolete code path whenever
auto-frameskip was enabled.  In that commit I changed how the frameskip
setting was read, using the config file rather than JNI. In that commit
I created a new config file field (maxFrameskip) but reused an existing
one (autoFrameskip). But that was a mistake because the autoFrameskip
field was already being used, in a way that had an entirely different
meaning.  Prior to that commit, autoFrameskip always had a value of 0
(since we didn't explicitly set it) which made gles2n64 use Yongzh's
FrameSkipper class (which encapsulates both auto and manual skipping).
When autoFrameskip was set to 1, gles2n64 used some legacy frameskip
algorithm that Yongzh's FrameSkipper class superseded.  So basically I
hijacked an existing flag and tried to give it a new meaning without
understanding its original meaning.

This commit fixes the regression.  To correct the problem and avoid any
further confusion, I just removed the legacy autoframeskip algorithm
altogether, always using Yongzh's much better algorithm, which is
encapsulated in the FrameSkipper class.

jni/gles2n64/src/gles2N64.cpp
@@ -138,33 +138,7 @@ EXPORT void CALL ProcessDList(void)
 {
     OGL.frame_dl++;
 
-    if (config.autoFrameSkip)
-    {
-        OGL_UpdateFrameTime();
-
-        if (OGL.consecutiveSkips < 1)
-        {
-            unsigned t = 0;
-            for(int i = 0; i < OGL_FRAMETIME_NUM; i++) t += OGL.frameTime[i];
-            t *= config.targetFPS;
-            if (config.romPAL) t = (t * 5) / 6;
-            if (t > (OGL_FRAMETIME_NUM * 1000))
-            {
-                OGL.consecutiveSkips++;
-                OGL.frameSkipped++;
-                RSP.busy = FALSE;
-                RSP.DList++;
-
-                /* avoid hang on frameskip */
-                *REG.MI_INTR |= MI_INTR_DP;
-                CheckInterrupts();
-                *REG.MI_INTR |= MI_INTR_SP;
-                CheckInterrupts();
-                return;
-            }
-        }
-    }
-    else if (frameSkipper.willSkipNext())
+    if (frameSkipper.willSkipNext())
     {
         OGL.frameSkipped++;
         RSP.busy = FALSE;