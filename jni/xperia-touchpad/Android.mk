3eba17a9f37c61c2dd9989c1811bd13b722e4d96
littleguy77 <mattlichter@live.com>
2013-09-24 21:00:45 -0400

    xperia-touchpad: Re-implement using android_native_app_glue.
    Major simplification of the native xperia-touchpad implementation.
    
    Not just a cosmetic/maintenance fix. This also fixes a thread orphaning
    bug.  Before, every time the user quit to the main menu from the game,
    one of the threads was not destroyed properly and was left dangling.
    So every time the user started and finished the game activity, another
    dead thread would be added to the process. We never noticed the problem
    before, since System.exit() force-killed these orphaned threads.  But
    with the new lifecycle fixes, this bug became evident.
    
    One note: If you build with NDK r9, the xperia touchpad and touchscreen
    will spam the logcat with spurious errors.  They're not really errors,
    but a new bug in the NDK.  If you build with NDK r8, you won't get the
    spam.  See here for details:
    http://stackoverflow.com/q/18316046/254218

--- jni/xperia-touchpad/Android.mk
+++ jni/xperia-touchpad/Android.mk
@@ -14,4 +14,8 @@ LOCAL_LDLIBS := \
     -landroid   \
     -llog       \
 
+LOCAL_STATIC_LIBRARIES := android_native_app_glue
+
 include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)
--- jni/xperia-touchpad/main.c
+++ jni/xperia-touchpad/main.c
@@ -19,24 +19,16 @@
  * Authors: littleguy77, Paul Lamb
  */
 
-#include <pthread.h>
 #include <jni.h>
 #include <android/log.h>
-#include <android/native_activity.h>
+#include <android_native_app_glue.h>
 
 #define TAG "xperia-touchpad"
-#define LOGV(...) ((void)__android_log_print( ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__ ))
 #define LOGI(...) ((void)__android_log_print( ANDROID_LOG_INFO, TAG, __VA_ARGS__ ))
-#define LOGW(...) ((void)__android_log_print( ANDROID_LOG_WARN, TAG, __VA_ARGS__ ))
 #define LOGE(...) ((void)__android_log_print( ANDROID_LOG_ERROR, TAG, __VA_ARGS__ ))
 
 #undef NUM_METHODS
 #define NUM_METHODS(x) (sizeof(x)/sizeof(*(x)))
-
-#define LOOPER_ID_INPUT		1
-
-#define MSG_INPUTQUEUE_CREATED		12
-#define MSG_INPUTQUEUE_DESTROYED	13
 
 #define MAX_POINTERS                64
 #define PAD_HEIGHT                  360
@@ -44,385 +36,186 @@
 #define SOURCE_TOUCHSCREEN          4098
 #define SOURCE_TOUCHPAD             1048584
 
-static jobject   g_pActivity        = 0;
-static jmethodID javaOnNativeKey    = 0;
-static jmethodID javaOnNativeTouch  = 0;
-static JNIEnv    *g_pEnv            = 0;
-static JavaVM    *g_pVM             = 0;
+static JNIEnv *g_pEnv = 0;
+static JavaVM *g_pVM = 0;
+static jobject g_pActivity = 0;
+static jmethodID javaOnNativeKey = 0;
+static jmethodID javaOnNativeTouch = 0;
 
-struct APP_MSG
+static int RegisterThis(JNIEnv* env, jobject clazz)
 {
-    char msg;
-    char arg1;
-};
-
-struct APP_INSTANCE
-{
-    // The application can place a pointer to its own state object here if it likes.
-    void* userData;
-
-    // The ANativeActivity object instance that this app is running in.
-    ANativeActivity* activity;
-
-    // The ALooper associated with the app's thread.
-    ALooper* looper;
-
-    // When non-NULL, this is the input queue from which the app will receive user input events.
-    AInputQueue* inputQueue;
-    AInputQueue* pendingInputQueue;
-
-    // Message queue.
-    struct APP_MSG msgQueue[512];
-    int msgQueueLength;
-
-    // Used to synchronize between callbacks and game thread.
-    pthread_mutex_t mutex;
-    pthread_cond_t cond;
-
-    // Game thread.
-    pthread_t thread;
-
-    // State flags.
-    int running;
-};
-
-/**
- * Our saved state data.
- */
-struct TOUCHSTATE
-{
-    int down;
-    int x;
-    int y;
-};
-
-/**
- * Shared state for our app.
- */
-struct ENGINE
-{
-    struct APP_INSTANCE* app;
-    struct TOUCHSTATE touchstate_screen[MAX_POINTERS];
-    struct TOUCHSTATE touchstate_pad[MAX_POINTERS];
-};
-
-static int32_t engine_handle_input( struct APP_INSTANCE* app, AInputEvent* event )
-{
-    struct ENGINE* engine = ( struct ENGINE* ) app->userData;
-
-    if( AInputEvent_getType( event ) == AINPUT_EVENT_TYPE_KEY )
-    {
-        int nKeyCode = AKeyEvent_getKeyCode( event );
-        int nAction = AKeyEvent_getAction( event );
-        int handled = 0;
-        if( g_pEnv && g_pActivity )
-            handled = ( *g_pEnv )->CallIntMethod( g_pEnv, g_pActivity, javaOnNativeKey, nAction, nKeyCode );
-        return handled;
-    }
-
-    else if( AInputEvent_getType( event ) == AINPUT_EVENT_TYPE_MOTION
-            && g_pEnv )
-    {
-        // TODO: This is probably inefficient; move some work to startup/shutdown methods
-
-        int action = AMotionEvent_getAction( event );
-        int source = AInputEvent_getSource( event );
-        int pointerCount = AMotionEvent_getPointerCount( event );
-
-        jintArray pointerIds = ( *g_pEnv )->NewIntArray( g_pEnv, MAX_POINTERS );
-        jfloatArray pointerX = ( *g_pEnv )->NewFloatArray( g_pEnv, MAX_POINTERS );
-        jfloatArray pointerY = ( *g_pEnv )->NewFloatArray( g_pEnv, MAX_POINTERS );
-
-        jint * tempIds = ( *g_pEnv )->GetIntArrayElements( g_pEnv, pointerIds, 0 );
-        jfloat * tempX = ( *g_pEnv )->GetFloatArrayElements( g_pEnv, pointerX, 0 );
-        jfloat * tempY = ( *g_pEnv )->GetFloatArrayElements( g_pEnv, pointerY, 0 );
-
-        int i;
-        for( i = 0; i < pointerCount; i++ )
-        {
-            tempIds[i] = AMotionEvent_getPointerId( event, i );
-            tempX[i] = AMotionEvent_getX( event, i );
-            if( source == SOURCE_TOUCHPAD )
-                tempY[i] = PAD_HEIGHT - AMotionEvent_getY( event, i );
-            else
-                tempY[i] = AMotionEvent_getY( event, i );
-        }
-
-        ( *g_pEnv )->ReleaseIntArrayElements( g_pEnv, pointerIds, tempIds, 0 );
-        ( *g_pEnv )->ReleaseFloatArrayElements( g_pEnv, pointerX, tempX, 0 );
-        ( *g_pEnv )->ReleaseFloatArrayElements( g_pEnv, pointerY, tempY, 0 );
-
-        int handled = 0;
-        if( g_pEnv && g_pActivity )
-            handled = ( *g_pEnv )->CallIntMethod( g_pEnv, g_pActivity, javaOnNativeTouch,
-                    source, action, pointerCount, pointerIds, pointerX, pointerY );
-
-        ( *g_pEnv )->DeleteLocalRef( g_pEnv, pointerIds );
-        ( *g_pEnv )->DeleteLocalRef( g_pEnv, pointerX );
-        ( *g_pEnv )->DeleteLocalRef( g_pEnv, pointerY );
-
-        return handled;
-    }
-    return 0;
-}
-
-static void app_lock_queue( struct APP_INSTANCE* state )
-{
-    pthread_mutex_lock( &state->mutex );
-}
-
-static void app_unlock_queue( struct APP_INSTANCE* state )
-{
-    pthread_cond_broadcast( &state->cond );
-    pthread_mutex_unlock( &state->mutex );
-}
-
-static void instance_app_main( struct APP_INSTANCE* app_instance )
-{
-    LOGI( "main entering." );
-
-    struct ENGINE engine;
-
-    memset( &engine, 0, sizeof( engine ) );
-    app_instance->userData = &engine;
-    engine.app = app_instance;
-
-    // Our 'main loop'
-    while( 1 )
-    {
-        // Read all pending events.
-        int msg_index;
-        int ident;
-        int events;
-        struct android_poll_source* source;
-
-        app_lock_queue( app_instance );
-
-        for( msg_index = 0; msg_index < app_instance->msgQueueLength; ++msg_index )
-        {
-            switch( app_instance->msgQueue[msg_index].msg )
-            {
-            case MSG_INPUTQUEUE_CREATED:
-            case MSG_INPUTQUEUE_DESTROYED:
-                if( app_instance->inputQueue != NULL )
-                    AInputQueue_detachLooper( app_instance->inputQueue );
-
-                app_instance->inputQueue = app_instance->pendingInputQueue;
-                if( app_instance->inputQueue != NULL )
-                {
-                    AInputQueue_attachLooper( app_instance->inputQueue,
-                            app_instance->looper, LOOPER_ID_INPUT, NULL, NULL );
-                }
-                break;
-            };
-        }
-
-        app_instance->msgQueueLength = 0;
-
-        app_unlock_queue( app_instance );
-
-        // If not rendering, we will block forever waiting for events.
-        // If rendering, we loop until all events are read, then continue
-        // to draw the next frame.
-        while( ( ident = ALooper_pollAll( 250, NULL, &events,
-                ( void** ) &source ) ) >= 0 )
-        {
-            if( ident == LOOPER_ID_INPUT )
-            {
-                AInputEvent* event = NULL;
-                if( AInputQueue_getEvent( app_instance->inputQueue, &event ) >= 0 )
-                {
-                    if( AInputQueue_preDispatchEvent( app_instance->inputQueue, event ) )
-                        continue;
-
-                    int handled = engine_handle_input( app_instance, event );
-
-                    AInputQueue_finishEvent( app_instance->inputQueue, event, handled );
-                }
-            }
-        }
-    }
-
-    LOGI( "main exiting." );
-}
-
-static
-void OnInputQueueCreated( ANativeActivity* activity, AInputQueue* queue )
-{
-    LOGI( "NativeActivity input queue created: %p -- %p\n", activity, queue );
-
-    struct APP_INSTANCE* app_instance = ( struct APP_INSTANCE* ) activity->instance;
-
-    pthread_mutex_lock( &app_instance->mutex );
-
-    app_instance->pendingInputQueue = queue;
-    app_instance->msgQueue[app_instance->msgQueueLength++].msg = MSG_INPUTQUEUE_CREATED;
-
-    while( app_instance->inputQueue != app_instance->pendingInputQueue )
-    {
-        pthread_cond_wait( &app_instance->cond, &app_instance->mutex );
-    }
-
-    app_instance->pendingInputQueue = NULL;
-
-    pthread_mutex_unlock( &app_instance->mutex );
-}
-
-static
-void OnInputQueueDestroyed( ANativeActivity* activity, AInputQueue* queue )
-{
-    LOGI( "NativeActivity input queue destroyed: %p -- %p\n", activity, queue );
-
-    struct APP_INSTANCE* app_instance = ( struct APP_INSTANCE* ) activity->instance;
-
-    pthread_mutex_lock( &app_instance->mutex );
-
-    app_instance->pendingInputQueue = NULL;
-    app_instance->msgQueue[app_instance->msgQueueLength++].msg = MSG_INPUTQUEUE_DESTROYED;
-
-    while( app_instance->inputQueue != app_instance->pendingInputQueue )
-    {
-        pthread_cond_wait( &app_instance->cond, &app_instance->mutex );
-    }
-
-    pthread_mutex_unlock( &app_instance->mutex );
-}
-
-static
-void*
-app_thread_entry( void* param )
-{
-    LOGI( "NativeActivity entered application thread" );
-
-    ( *g_pVM )->AttachCurrentThread( g_pVM, &g_pEnv, 0 );
-
-    struct APP_INSTANCE* app_instance = ( struct APP_INSTANCE* ) param;
-
-    // Create/get a looper
-    ALooper* looper = ALooper_prepare( ALOOPER_PREPARE_ALLOW_NON_CALLBACKS );
-    app_instance->looper = looper;
-
-    // Tell the thread which created this one that we are now up and running
-    pthread_mutex_lock( &app_instance->mutex );
-    app_instance->running = 1;
-    pthread_cond_broadcast( &app_instance->cond );
-    pthread_mutex_unlock( &app_instance->mutex );
-
-    // Run the game
-    instance_app_main( app_instance );
-
-    pthread_mutex_lock( &app_instance->mutex );
-
-    if( app_instance->inputQueue != NULL )
-    {
-        AInputQueue_detachLooper( app_instance->inputQueue );
-    }
-
-    pthread_mutex_unlock( &app_instance->mutex );
-    pthread_mutex_destroy( &app_instance->mutex );
-
-    free( app_instance );
-
-    ( *g_pVM )->DetachCurrentThread( g_pVM );
-
-    LOGI( "NativeActivity exiting application thread" );
-
-    return NULL;
-}
-
-// Entry point from android/nativeactivity
-JNIEXPORT void JNICALL
-ANativeActivity_onCreate( ANativeActivity* activity, void* saved_state, size_t saved_state_size )
-{
-    LOGI( "NativeActivity creating: %p\n", activity );
-
-    activity->callbacks->onInputQueueCreated = OnInputQueueCreated;
-    activity->callbacks->onInputQueueDestroyed = OnInputQueueDestroyed;
-
-    struct APP_INSTANCE* app_instance = ( struct APP_INSTANCE* ) malloc( sizeof(struct APP_INSTANCE) );
-    memset( app_instance, 0, sizeof(struct APP_INSTANCE) );
-    app_instance->activity = activity;
-
-    pthread_mutex_init( &app_instance->mutex, NULL );
-    pthread_cond_init( &app_instance->cond, NULL );
-
-    pthread_attr_t attr;
-    pthread_attr_init( &attr );
-    pthread_attr_setdetachstate( &attr, PTHREAD_CREATE_DETACHED );
-    pthread_create( &app_instance->thread, &attr, app_thread_entry, app_instance );
-
-    // Wait for thread to start.
-    pthread_mutex_lock( &app_instance->mutex );
-    while( !app_instance->running )
-    {
-        pthread_cond_wait( &app_instance->cond, &app_instance->mutex );
-    }
-    pthread_mutex_unlock( &app_instance->mutex );
-
-    activity->instance = app_instance;
-}
-
-static
-int RegisterThis( JNIEnv* env, jobject clazz )
-{
-    LOGI( "RegisterThis() was called" );
-    g_pActivity = ( jobject )( *env )->NewGlobalRef( env, clazz );
+    LOGI("RegisterThis() was called");
+    g_pActivity = (jobject)(*env)->NewGlobalRef(env, clazz);
 
     return 0;
 }
 
 static const JNINativeMethod activity_methods[] =
-    { { "RegisterThis", "()I", ( void* ) RegisterThis }, };
+{
+{ "RegisterThis", "()I", (void*) RegisterThis } };
 
-JNIEXPORT jint JNICALL
-JNI_OnLoad( JavaVM * vm, void * reserved )
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM * vm, void * reserved)
 {
     JNIEnv* env = 0;
     g_pVM = vm;
 
-    if( ( *vm )->GetEnv( vm, ( void** ) &env, JNI_VERSION_1_4 ) != JNI_OK )
+    if ((*vm)->GetEnv(vm, (void**) &env, JNI_VERSION_1_4) != JNI_OK)
     {
-        LOGE( "%s - Failed to get the environment using GetEnv()", __FUNCTION__ );
+        LOGE("%s - Failed to get the environment using GetEnv()", __FUNCTION__);
         return -1;
     }
 
     const char* interface_path = "paulscode/android/mupen64plusae/jni/NativeXperiaTouchpad";
-    jclass java_activity_class = ( *env )->FindClass( env, interface_path );
+    jclass java_activity_class = (*env)->FindClass(env, interface_path);
 
-    if( !java_activity_class )
+    if (!java_activity_class)
     {
-        LOGE( "%s - Failed to get %s class reference", __FUNCTION__, interface_path );
+        LOGE("%s - Failed to get %s class reference", __FUNCTION__, interface_path);
         return -1;
     }
 
-    if( ( *env )->RegisterNatives( env, java_activity_class, activity_methods, NUM_METHODS(activity_methods) ) != JNI_OK )
+    if ((*env)->RegisterNatives(env, java_activity_class, activity_methods, NUM_METHODS(activity_methods)) != JNI_OK)
     {
-        LOGE( "%s - Failed to register native activity methods", __FUNCTION__ );
+        LOGE("%s - Failed to register native activity methods", __FUNCTION__);
         return -1;
     }
 
-    javaOnNativeKey = ( *env )->GetMethodID( env, java_activity_class, "onNativeKey", "(II)Z" );
-    if( !javaOnNativeKey )
+    javaOnNativeKey = (*env)->GetMethodID(env, java_activity_class, "onNativeKey", "(II)Z");
+    if (!javaOnNativeKey)
     {
-        if( ( *env )->ExceptionCheck( env ) )
+        if ((*env)->ExceptionCheck(env))
         {
-            LOGE( "%s - GetMethodID( 'onNativeKey' ) threw exception!", __FUNCTION__ );
-            ( *env )->ExceptionClear( env );
+            LOGE("%s - GetMethodID( 'onNativeKey' ) threw exception!", __FUNCTION__);
+            (*env)->ExceptionClear(env);
         }
         return JNI_FALSE;
     }
-    javaOnNativeTouch = ( *env )->GetMethodID( env, java_activity_class, "onNativeTouch", "(III[I[F[F)Z" );
-    if( !javaOnNativeTouch )
+    javaOnNativeTouch = (*env)->GetMethodID(env, java_activity_class, "onNativeTouch", "(III[I[F[F)Z");
+    if (!javaOnNativeTouch)
     {
-        if( ( *env )->ExceptionCheck( env ) )
+        if ((*env)->ExceptionCheck(env))
         {
-            LOGE( "%s - GetMethodID( 'onNativeTouch' ) threw exception!", __FUNCTION__ );
-            ( *env )->ExceptionClear( env );
+            LOGE("%s - GetMethodID( 'onNativeTouch' ) threw exception!", __FUNCTION__);
+            (*env)->ExceptionClear(env);
         }
         return JNI_FALSE;
     }
 
-    LOGI( "%s - Complete", __FUNCTION__ );
+    LOGI("%s - Complete", __FUNCTION__);
 
     return JNI_VERSION_1_4;
+}
+
+/**
+ * Process the next input event.
+ */
+static int32_t onInputEvent(struct android_app* app, AInputEvent* event)
+{
+    if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_KEY)
+    {
+        int nKeyCode = AKeyEvent_getKeyCode(event);
+        int nAction = AKeyEvent_getAction(event);
+        int handled = 0;
+        if (g_pEnv && g_pActivity)
+            handled = (*g_pEnv)->CallIntMethod(g_pEnv, g_pActivity, javaOnNativeKey, nAction, nKeyCode);
+        return handled;
+    }
+
+    else if (AInputEvent_getType(event) == AINPUT_EVENT_TYPE_MOTION && g_pEnv)
+    {
+        int action = AMotionEvent_getAction(event);
+        int source = AInputEvent_getSource(event);
+        int pointerCount = AMotionEvent_getPointerCount(event);
+
+        jintArray pointerIds = (*g_pEnv)->NewIntArray(g_pEnv, MAX_POINTERS);
+        jfloatArray pointerX = (*g_pEnv)->NewFloatArray(g_pEnv, MAX_POINTERS);
+        jfloatArray pointerY = (*g_pEnv)->NewFloatArray(g_pEnv, MAX_POINTERS);
+
+        jint * tempIds = (*g_pEnv)->GetIntArrayElements(g_pEnv, pointerIds, 0);
+        jfloat * tempX = (*g_pEnv)->GetFloatArrayElements(g_pEnv, pointerX, 0);
+        jfloat * tempY = (*g_pEnv)->GetFloatArrayElements(g_pEnv, pointerY, 0);
+
+        int i;
+        for (i = 0; i < pointerCount; i++)
+        {
+            tempIds[i] = AMotionEvent_getPointerId(event, i);
+            tempX[i] = AMotionEvent_getX(event, i);
+            if (source == SOURCE_TOUCHPAD)
+                tempY[i] = PAD_HEIGHT - AMotionEvent_getY(event, i);
+            else
+                tempY[i] = AMotionEvent_getY(event, i);
+        }
+
+        (*g_pEnv)->ReleaseIntArrayElements(g_pEnv, pointerIds, tempIds, 0);
+        (*g_pEnv)->ReleaseFloatArrayElements(g_pEnv, pointerX, tempX, 0);
+        (*g_pEnv)->ReleaseFloatArrayElements(g_pEnv, pointerY, tempY, 0);
+
+        int handled = 0;
+        if (g_pEnv && g_pActivity)
+            handled = (*g_pEnv)->CallIntMethod(g_pEnv, g_pActivity, javaOnNativeTouch, source, action, pointerCount, pointerIds, pointerX, pointerY);
+
+        (*g_pEnv)->DeleteLocalRef(g_pEnv, pointerIds);
+        (*g_pEnv)->DeleteLocalRef(g_pEnv, pointerX);
+        (*g_pEnv)->DeleteLocalRef(g_pEnv, pointerY);
+
+        return handled;
+    }
+    return 0;
+}
+
+/**
+ * Process the next application command.
+ */
+static void onAppCmd(struct android_app* app, int32_t cmd)
+{
+    switch (cmd)
+    {
+    case APP_CMD_RESUME:
+        if (g_pVM)
+            (*g_pVM)->AttachCurrentThread(g_pVM, &g_pEnv, 0);
+        break;
+    case APP_CMD_PAUSE:
+        if (g_pVM)
+            (*g_pVM)->DetachCurrentThread(g_pVM);
+        break;
+    }
+}
+
+/**
+ * This is the main entry point of a native application that is using
+ * android_native_app_glue.  It runs in its own thread, with its own
+ * event loop for receiving input events and application messages.
+ */
+void android_main(struct android_app* state)
+{
+    // Make sure glue isn't stripped
+    app_dummy();
+
+    // Register callback functions
+    state->onAppCmd = onAppCmd;
+    state->onInputEvent = onInputEvent;
+
+    // Main loop
+    while (1)
+    {
+        // Read all pending events
+        int ident;
+        int events;
+        struct android_poll_source* source;
+
+        while ((ident = ALooper_pollAll(250, NULL, &events, (void**) &source)) >= 0)
+        {
+            // Notify glue code to call onInputEvent
+            if (source != NULL)
+            {
+                source->process(state, source);
+            }
+
+            // Exit main loop if necessary
+            if (state->destroyRequested != 0)
+            {
+                return;
+            }
+        }
+    }
 }
