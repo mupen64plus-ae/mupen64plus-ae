mupen64plus-ae/mupen64plus-ae

commit 48e94450531fe6af307737cb3606b492e188d569

core: Remove Android customization, revert to upstream.
These custom hard-coded paths are no longer needed since we're now
passing them via the config file.  Using the original upstream values
doesn't hurt anything.

jni/core/src/osal/files_unix.c
@@ -70,23 +70,13 @@ bool macSetBundlePath(char* buffer)
 #if defined(SHAREDIR)
   #define XSTR(S) STR(S) /* this wacky preprocessor thing is necessary to generate a quote-enclosed */
   #define STR(S) #S      /* copy of the SHAREDIR macro, which is defined by the makefile via gcc -DSHAREDIR="..." */
-  #ifdef ANDROID
-  static const int   datasearchdirs = 3;
-  static const char *datasearchpath[3] = { XSTR(SHAREDIR), "data/",  "./" };
-  #else
   static const int   datasearchdirs = 4;
   static const char *datasearchpath[4] = { XSTR(SHAREDIR), "/usr/local/share/mupen64plus",  "/usr/share/mupen64plus", "./" };
-  #endif
   #undef STR
   #undef XSTR
 #else
-  #ifdef ANDROID
-  static const int   datasearchdirs = 2;
-  static const char *datasearchpath[2] = { "data/",  "./" };
-  #else
   static const int   datasearchdirs = 3;
   static const char *datasearchpath[3] = { "/usr/local/share/mupen64plus",  "/usr/share/mupen64plus", "./" };
-  #endif
 #endif
 
 /* local functions */
@@ -225,20 +215,12 @@ const char * osal_get_user_configpath(void)
     int rval;
     
     /* first, try the XDG_CONFIG_HOME environment variable */
-    #ifdef ANDROID
-    rval = get_xdg_dir(retpath, "XDG_CONFIG_HOME", "data/");
-    #else
     rval = get_xdg_dir(retpath, "XDG_CONFIG_HOME", "mupen64plus/");
-    #endif
     if (rval == 0)
         return retpath;
 
     /* then try the HOME environment variable */
-    #ifdef ANDROID
-    rval = get_xdg_dir(retpath, "HOME", "./");
-    #else
     rval = get_xdg_dir(retpath, "HOME", ".config/mupen64plus/");
-    #endif
     if (rval == 0)
         return retpath;
 
@@ -254,20 +236,12 @@ const char * osal_get_user_datapath(void)
     int rval;
     
     /* first, try the XDG_DATA_HOME environment variable */
-    #ifdef ANDROID
-    rval = get_xdg_dir(retpath, "XDG_DATA_HOME", "data/");
-    #else
     rval = get_xdg_dir(retpath, "XDG_DATA_HOME", "mupen64plus/");
-    #endif
     if (rval == 0)
         return retpath;
 
     /* then try the HOME environment variable */
-    #ifdef ANDROID
-    rval = get_xdg_dir(retpath, "HOME", "./");
-    #else
     rval = get_xdg_dir(retpath, "HOME", ".local/share/mupen64plus/");
-    #endif
     if (rval == 0)
         return retpath;
 
@@ -283,20 +257,12 @@ const char * osal_get_user_cachepath(void)
     int rval;
     
     /* first, try the XDG_CACHE_HOME environment variable */
-    #ifdef ANDROID
-    rval = get_xdg_dir(retpath, "XDG_CACHE_HOME", "data/");
-    #else
     rval = get_xdg_dir(retpath, "XDG_CACHE_HOME", "mupen64plus/");
-    #endif
     if (rval == 0)
         return retpath;
 
     /* then try the HOME environment variable */
-    #ifdef ANDROID
-    rval = get_xdg_dir(retpath, "HOME", "./");
-    #else
     rval = get_xdg_dir(retpath, "HOME", ".cache/mupen64plus/");
-    #endif
     if (rval == 0)
         return retpath;
 

commit 0810a2252db5a06ecf0602dce0478add5eedd162

bugfix: Fix filepath regression (e.g. rice hi-res textures).
Bug introduced in commit
48e94450531fe6af307737cb3606b492e188d569

which broke two methods in the core API:

   - ConfigGetUserDataPath
   - ConfigGetUserCachePath

These paths can only be resolved through the Android customizations
that we've been using all along.  It wasn't obvious, however, because
the only code that calls these methods are:
   - Rice uses ConfigGetUserDataPath for hi-res textures & tex dumps
   - Glide uses both, but only if TEXTURE_FILTER is defined (which it
     currently isn't)

Note that ConfigGetUserConfigPath and ConfigGetSharedDataPath never
broke because they are obtained by the core via command-line arg and
config file, respectively.

jni/core/src/osal/files_unix.c
@@ -236,7 +236,11 @@ const char * osal_get_user_datapath(void)
     int rval;
     
     /* first, try the XDG_DATA_HOME environment variable */
+    #ifdef ANDROID
+    rval = get_xdg_dir(retpath, "XDG_DATA_HOME", "data/");
+    #else
     rval = get_xdg_dir(retpath, "XDG_DATA_HOME", "mupen64plus/");
+    #endif
     if (rval == 0)
         return retpath;
 
@@ -257,7 +261,11 @@ const char * osal_get_user_cachepath(void)
     int rval;
     
     /* first, try the XDG_CACHE_HOME environment variable */
+    #ifdef ANDROID
+    rval = get_xdg_dir(retpath, "XDG_CACHE_HOME", "data/");
+    #else
     rval = get_xdg_dir(retpath, "XDG_CACHE_HOME", "mupen64plus/");
+    #endif
     if (rval == 0)
         return retpath;
 