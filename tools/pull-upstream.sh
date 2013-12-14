4d8b60adf452b0a846dd78ee52170e4452b25cf1
Sven Eckelmann <sven@narfation.org>
2013-12-14 17:57:25 +0100

    tools: Add an example how the upstream source can be pulled

---
+++ tools/pull-upstream.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+set -e
+
+# This is more an example how to merge upstream sources directly than an
+# actual script because it breaks quite easily when detecting a merge problem.
+
+# From https://github.com/git/git/blob/master/contrib/subtree/git-subtree.sh
+GITSUBTREE=/usr/share/doc/git/contrib/subtree/git-subtree.sh
+
+sh ${GITSUBTREE} pull --prefix=jni/audio-sdl https://bitbucket.org/mupen64plus/mupen64plus-audio-sdl.git master
+sh ${GITSUBTREE} pull --prefix=jni/core https://bitbucket.org/mupen64plus/mupen64plus-core.git master
+sh ${GITSUBTREE} pull --prefix=jni/rsp-hle https://bitbucket.org/mupen64plus/mupen64plus-rsp-hle.git master
+sh ${GITSUBTREE} pull --prefix=jni/front-end https://bitbucket.org/mupen64plus/mupen64plus-ui-console.git master
+sh ${GITSUBTREE} pull --prefix=jni/gles2glide64 https://bitbucket.org/mupen64plus/mupen64plus-video-glide64mk2.git master
+sh ${GITSUBTREE} pull --prefix=jni/gles2rice https://bitbucket.org/mupen64plus/mupen64plus-video-rice.git master
+
+echo "Finished"
