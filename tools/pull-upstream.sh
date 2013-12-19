1d948742b7a91e9de27bf610d82ffd93c5e89ca0
Sven Eckelmann <sven@narfation.org>
2013-12-19 15:12:05 +0100

    tools: Use the mapping from diff-upstream in pull-upstream

--- tools/pull-upstream.sh
+++ tools/pull-upstream.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+M64P_COMPONENTS="core ui-console rsp-hle audio-sdl video-rice video-glide64mk2"
+
 # This is more an example how to merge upstream sources directly than an
 # actual script because it breaks quite easily when detecting a merge problem.
 
@@ -10,11 +12,24 @@ curl https://raw.github.com/git/git/master/contrib/subtree/git-subtree.sh > git-
 cd ..
 GITSUBTREE=./tools/git-subtree.sh
 
-sh ${GITSUBTREE} pull --prefix=jni/audio-sdl https://github.com/mupen64plus/mupen64plus-audio-sdl.git master
-sh ${GITSUBTREE} pull --prefix=jni/core https://github.com/mupen64plus/mupen64plus-core.git master
-sh ${GITSUBTREE} pull --prefix=jni/rsp-hle https://github.com/mupen64plus/mupen64plus-rsp-hle.git master
-sh ${GITSUBTREE} pull --prefix=jni/front-end https://github.com/mupen64plus/mupen64plus-ui-console.git master
-sh ${GITSUBTREE} pull --prefix=jni/gles2glide64 https://github.com/mupen64plus/mupen64plus-video-glide64mk2.git master
-sh ${GITSUBTREE} pull --prefix=jni/gles2rice https://github.com/mupen64plus/mupen64plus-video-rice.git master
+for i in $M64P_COMPONENTS; do
+
+    case "$i" in
+    "ui-console")
+        ae_module="front-end"
+        ;;
+    "video-rice")
+        ae_module="gles2rice"
+        ;;
+    "video-glide64mk2")
+        ae_module="gles2glide64"
+        ;;
+    *)
+        ae_module="${i}"
+        ;;
+    esac
+
+    sh ${GITSUBTREE} pull --prefix="jni/${ae_module}" https://github.com/mupen64plus/mupen64plus-"${i}".git master
+done
 
 echo "Finished"
