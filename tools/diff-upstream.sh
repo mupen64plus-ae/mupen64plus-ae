e3a3660e3a498da67007a6f31dd0dd43dd3a74d6
Sven Eckelmann <sven@narfation.org>
2013-12-19 14:43:36 +0100

    tools: Use new github mupen64plus upstream repositories

--- tools/diff-upstream.sh
+++ tools/diff-upstream.sh
@@ -5,33 +5,38 @@ mkdir -p tmp/upstream
 cd tmp/upstream/
 
 if [ -d "mupen64plus-core" ]; then
-    hg pull -u mupen64plus-core
+    path="$(pwd)/mupen64plus-core/"
+    git --work-tree="${path}" --git-dir="${path}/.git" pull --ff-only
 else
-    hg clone https://bitbucket.org/richard42/mupen64plus-core
+    git clone https://github.com/mupen64plus/mupen64plus-core
 fi
 
 if [ -d "mupen64plus-ui-console" ]; then
-    hg pull -u mupen64plus-ui-console
+    path="$(pwd)/mupen64plus-ui-console/"
+    git --work-tree="${path}" --git-dir="${path}/.git" pull --ff-only
 else
-    hg clone https://bitbucket.org/richard42/mupen64plus-ui-console
+    git clone https://github.com/mupen64plus/mupen64plus-ui-console
 fi
 
 if [ -d "mupen64plus-rsp-hle" ]; then
-    hg pull -u mupen64plus-rsp-hle
+    path="$(pwd)/mupen64plus-rsp-hle/"
+    git --work-tree="${path}" --git-dir="${path}/.git" pull --ff-only
 else
-    hg clone https://bitbucket.org/richard42/mupen64plus-rsp-hle
+    git clone https://github.com/mupen64plus/mupen64plus-rsp-hle
 fi
 
 if [ -d "mupen64plus-audio-sdl" ]; then
-    hg pull -u mupen64plus-audio-sdl
+    path="$(pwd)/mupen64plus-audio-sdl/"
+    git --work-tree="${path}" --git-dir="${path}/.git" pull --ff-only
 else
-    hg clone https://bitbucket.org/richard42/mupen64plus-audio-sdl
+    git clone https://github.com/mupen64plus/mupen64plus-audio-sdl
 fi
 
 if [ -d "mupen64plus-video-rice" ]; then
-    hg pull -u mupen64plus-video-rice
+    path="$(pwd)/mupen64plus-video-rice/"
+    git --work-tree="${path}" --git-dir="${path}/.git" pull --ff-only
 else
-    hg clone https://bitbucket.org/richard42/mupen64plus-video-rice
+    git clone https://github.com/mupen64plus/mupen64plus-video-rice
 fi
 
 cd ../../
--- tools/pull-upstream.sh
+++ tools/pull-upstream.sh
@@ -10,11 +10,11 @@ curl https://raw.github.com/git/git/master/contrib/subtree/git-subtree.sh > git-
 cd ..
 GITSUBTREE=./tools/git-subtree.sh
 
-sh ${GITSUBTREE} pull --prefix=jni/audio-sdl https://bitbucket.org/mupen64plus/mupen64plus-audio-sdl.git master
-sh ${GITSUBTREE} pull --prefix=jni/core https://bitbucket.org/mupen64plus/mupen64plus-core.git master
-sh ${GITSUBTREE} pull --prefix=jni/rsp-hle https://bitbucket.org/mupen64plus/mupen64plus-rsp-hle.git master
-sh ${GITSUBTREE} pull --prefix=jni/front-end https://bitbucket.org/mupen64plus/mupen64plus-ui-console.git master
-sh ${GITSUBTREE} pull --prefix=jni/gles2glide64 https://bitbucket.org/mupen64plus/mupen64plus-video-glide64mk2.git master
-sh ${GITSUBTREE} pull --prefix=jni/gles2rice https://bitbucket.org/mupen64plus/mupen64plus-video-rice.git master
+sh ${GITSUBTREE} pull --prefix=jni/audio-sdl https://github.com/mupen64plus/mupen64plus-audio-sdl.git master
+sh ${GITSUBTREE} pull --prefix=jni/core https://github.com/mupen64plus/mupen64plus-core.git master
+sh ${GITSUBTREE} pull --prefix=jni/rsp-hle https://github.com/mupen64plus/mupen64plus-rsp-hle.git master
+sh ${GITSUBTREE} pull --prefix=jni/front-end https://github.com/mupen64plus/mupen64plus-ui-console.git master
+sh ${GITSUBTREE} pull --prefix=jni/gles2glide64 https://github.com/mupen64plus/mupen64plus-video-glide64mk2.git master
+sh ${GITSUBTREE} pull --prefix=jni/gles2rice https://github.com/mupen64plus/mupen64plus-video-rice.git master
 
 echo "Finished"
