03461f46e0eff4c18565e408bfb7307ed8548677
littleguy77 <mattlichter@live.com>
2013-12-26 09:08:27 -0500

    front: Add AppData field for mupencheat.txt file.

--- src/paulscode/android/mupen64plusae/persistent/AppData.java
+++ src/paulscode/android/mupen64plusae/persistent/AppData.java
@@ -155,8 +155,16 @@ public class AppData
     /** The path of the gles2glide64 configuration file. Deleted on uninstall, sometimes overwritten on update. */
     public final String gles2glide64_conf;
     
-    /** The path of the Mupen64Plus cheats file. Deleted on uninstall, sometimes overwritten on update. */
+    /**
+     * The path of the deprecated Mupen64Plus cheats file. Deleted on uninstall, sometimes
+     * overwritten on update.
+     * 
+     * @deprecated
+     */
     public final String mupen64plus_cht;
+    
+    /** The path of the Mupen64Plus cheats file. Deleted on uninstall, sometimes overwritten on update. */
+    public final String mupencheat_txt;
     
     /** The path of the Mupen64Plus ini file. Deleted on uninstall, sometimes overwritten on update. */
     public final String mupen64plus_ini;
@@ -238,6 +246,7 @@ public class AppData
         gles2n64_conf = coreSharedDataDir + "/gles2n64.conf";
         gles2glide64_conf = coreSharedDataDir + "/Glide64mk2.ini";
         mupen64plus_cht = coreSharedDataDir + "/mupen64plus.cht";
+        mupencheat_txt = coreSharedDataDir + "/mupencheat.txt";
         mupen64plus_ini = coreSharedDataDir + "/mupen64plus.ini";
         controllerProfiles_cfg = profilesDir + "/controller.cfg";
         touchscreenProfiles_cfg = profilesDir + "/touchscreen.cfg";
