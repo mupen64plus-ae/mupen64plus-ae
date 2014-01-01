mupen64plus-ae/mupen64plus-ae

commit 17539be993ccbd44d3ff60b0e3423a3b287e96f6

front: Move some app-wide initialization stuff to SplashActivity.
SplashActivity is the first activity launched, so do all one-time-only/
constant data initializations there.

Basically trying to thin down MenuActivity as much as possible so that
it's easier to implement the new front screen for version 3.

src/paulscode/android/mupen64plusae/SplashActivity.java
@@ -30,9 +30,13 @@ import paulscode.android.mupen64plusae.util.AssetExtractor.ExtractionFailure;
 import paulscode.android.mupen64plusae.util.AssetExtractor.OnExtractionProgressListener;
 import paulscode.android.mupen64plusae.util.FileUtil;
 import paulscode.android.mupen64plusae.util.Notifier;
+import paulscode.android.mupen64plusae.util.OUYAInterface;
+import paulscode.android.mupen64plusae.util.PrefUtil;
+import paulscode.android.mupen64plusae.util.RomDetail;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences.Editor;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -66,14 +70,32 @@ public class SplashActivity extends Activity implements OnExtractionProgressList
      */
     private static final String SOURCE_DIR = "mupen64plus_data";
     
-    /** Persistent application data. */
-    private AppData mAppData;
-    
     /** The text view that displays extraction progress info. */
     private TextView mTextView;
     
     /** The running count of assets extracted. */
     private int mAssetsExtracted;
+    
+    // App data and user preferences
+    private AppData mAppData = null;
+    private UserPrefs mUserPrefs = null;
+    private SharedPreferences mPrefs = null;
+    
+    // These constants must match the keys used in res/xml/preferences*.xml
+    private static final String TOUCHSCREEN_ENABLED = "touchscreenEnabled";
+    private static final String TOUCHSCREEN_STYLE = "touchscreenStyle";
+    private static final String TOUCHSCREEN_HEIGHT = "touchscreenHeight";
+    private static final String TOUCHSCREEN_LAYOUT = "touchscreenLayout";
+    private static final String TOUCHPAD_ENABLED = "touchpadEnabled";
+    private static final String TOUCHPAD_LAYOUT = "touchpadLayout";
+    private static final String INPUT_VOLUME_MAPPABLE = "inputVolumeMappable";
+    private static final String DISPLAY_POSITION = "displayPosition";
+    private static final String DISPLAY_RESOLUTION = "displayResolution";
+    private static final String DISPLAY_SCALING = "displayScaling";
+    private static final String NAVIGATION_MODE = "navigationMode";
+    private static final String R4300_EMULATOR = "r4300Emulator";
+    private static final String AUDIO_PLUGIN = "audioPlugin";
+    private static final String AUDIO_BUFFER_SIZE = "audioBufferSize";
     
     /*
      * (non-Javadoc)
@@ -85,19 +107,59 @@ public class SplashActivity extends Activity implements OnExtractionProgressList
     {
         super.onCreate( savedInstanceState );
         
+        // Get app data and user preferences
+        mAppData = new AppData( this );
+        mUserPrefs = new UserPrefs( this );
+        mUserPrefs.enforceLocale( this );
+        mPrefs = PreferenceManager.getDefaultSharedPreferences( this );
+        
         // Save the path of the ROM, if it was passed to the activity
         Uri dataUri = this.getIntent().getData();
         if( dataUri != null )
+            mPrefs.edit().putString( "pathSelectedGame", dataUri.getPath() ).commit();
+        
+        // Disable the Xperia PLAY plugin as necessary
+        if( !mAppData.hardwareInfo.isXperiaPlay )
+            mPrefs.edit().putBoolean( TOUCHPAD_ENABLED, false ).commit();
+        
+        // Set some prefs when running in big-screen mode
+        if( mUserPrefs.isBigScreenMode )
         {
-            Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
-            editor.putString( "pathSelectedGame", dataUri.getPath() ).commit();
+            mPrefs.edit().putBoolean( TOUCHSCREEN_ENABLED, false ).commit();
+            mPrefs.edit().putBoolean( INPUT_VOLUME_MAPPABLE, true ).commit();
         }
         
-        // Enforce any locale overrides
-        new UserPrefs( this ).enforceLocale( this );
+        // Ensure that any missing preferences are populated with defaults (e.g. preference added to new release)
+        PreferenceManager.setDefaultValues( this, R.xml.preferences, false );
+        PreferenceManager.setDefaultValues( this, R.xml.preferences_global, false );
+        PreferenceManager.setDefaultValues( this, R.xml.preferences_play, false );
+        PreferenceManager.setDefaultValues( this, R.xml.preferences_video, false );
         
-        // Get app data
-        mAppData = new AppData( this );
+        // Ensure that selected plugin names and other list preferences are valid
+        // @formatter:off
+        Resources res = getResources();
+        PrefUtil.validateListPreference( res, mPrefs, TOUCHSCREEN_STYLE,  R.string.touchscreenStyle_default,  R.array.touchscreenStyle_values );
+        PrefUtil.validateListPreference( res, mPrefs, TOUCHSCREEN_HEIGHT, R.string.touchscreenHeight_default, R.array.touchscreenHeight_values );
+        PrefUtil.validateListPreference( res, mPrefs, TOUCHSCREEN_LAYOUT, R.string.touchscreenLayout_default, R.array.touchscreenLayout_values );
+        PrefUtil.validateListPreference( res, mPrefs, TOUCHPAD_LAYOUT,    R.string.touchpadLayout_default,    R.array.touchpadLayout_values );
+        PrefUtil.validateListPreference( res, mPrefs, DISPLAY_POSITION,   R.string.displayPosition_default,   R.array.displayPosition_values );
+        PrefUtil.validateListPreference( res, mPrefs, DISPLAY_RESOLUTION, R.string.displayResolution_default, R.array.displayResolution_values );
+        PrefUtil.validateListPreference( res, mPrefs, DISPLAY_SCALING,    R.string.displayScaling_default,    R.array.displayScaling_values );
+        PrefUtil.validateListPreference( res, mPrefs, AUDIO_PLUGIN,       R.string.audioPlugin_default,       R.array.audioPlugin_values );
+        PrefUtil.validateListPreference( res, mPrefs, AUDIO_BUFFER_SIZE,  R.string.audioBufferSize_default,   R.array.audioBufferSize_values );
+        PrefUtil.validateListPreference( res, mPrefs, R4300_EMULATOR,     R.string.r4300Emulator_default,     R.array.r4300Emulator_values );
+        PrefUtil.validateListPreference( res, mPrefs, NAVIGATION_MODE,    R.string.navigationMode_default,    R.array.navigationMode_values );
+        // @formatter:on
+        
+        // Refresh the preference data wrapper
+        mUserPrefs = new UserPrefs( this );
+        
+        // Initialize ROM database
+        RomDetail.initializeDatabase( mAppData.mupen64plus_ini );
+        
+        // Initialize the OUYA interface if running on OUYA
+        if( OUYAInterface.IS_OUYA_HARDWARE )
+            OUYAInterface.init( this );
         
         // Initialize the toast/status bar notifier
         Notifier.initialize( this );
@@ -109,7 +171,7 @@ public class SplashActivity extends Activity implements OnExtractionProgressList
         setContentView( R.layout.main_activity );
         mTextView = (TextView) findViewById( R.id.mainText );
         
-        if( new UserPrefs( this ).isBigScreenMode )
+        if( mUserPrefs.isBigScreenMode )
         {
             ImageView splash = (ImageView) findViewById( R.id.mainImage );
             splash.setImageResource( R.drawable.publisherlogo_ouya );

src/paulscode/android/mupen64plusae/MenuActivity.java
@@ -29,8 +29,6 @@ import paulscode.android.mupen64plusae.persistent.UserPrefs;
 import paulscode.android.mupen64plusae.util.ChangeLog;
 import paulscode.android.mupen64plusae.util.DeviceUtil;
 import paulscode.android.mupen64plusae.util.Notifier;
-import paulscode.android.mupen64plusae.util.OUYAInterface;
-import paulscode.android.mupen64plusae.util.PrefUtil;
 import paulscode.android.mupen64plusae.util.RomDetail;
 import paulscode.android.mupen64plusae.util.Utility;
 import android.annotation.TargetApi;
@@ -38,7 +36,6 @@ import android.app.AlertDialog.Builder;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -53,23 +50,7 @@ import android.widget.ImageView;
 public class MenuActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener
 {
     // These constants must match the keys used in res/xml/preferences.xml
-    
     private static final String PATH_SELECTED_GAME = "pathSelectedGame";
-    
-    private static final String TOUCHSCREEN_ENABLED = "touchscreenEnabled";
-    private static final String TOUCHSCREEN_STYLE = "touchscreenStyle";
-    private static final String TOUCHSCREEN_HEIGHT = "touchscreenHeight";
-    private static final String TOUCHSCREEN_LAYOUT = "touchscreenLayout";
-    private static final String TOUCHPAD_ENABLED = "touchpadEnabled";
-    private static final String TOUCHPAD_LAYOUT = "touchpadLayout";
-    private static final String INPUT_VOLUME_MAPPABLE = "inputVolumeMappable";
-    private static final String DISPLAY_POSITION = "displayPosition";
-    private static final String DISPLAY_RESOLUTION = "displayResolution";
-    private static final String DISPLAY_SCALING = "displayScaling";
-    private static final String NAVIGATION_MODE = "navigationMode";
-    private static final String R4300_EMULATOR = "r4300Emulator";
-    private static final String AUDIO_PLUGIN = "audioPlugin";
-    private static final String AUDIO_BUFFER_SIZE = "audioBufferSize";
     private static final String LOCALE_OVERRIDE = "localeOverride";
     
     // App data and user preferences
@@ -106,7 +87,6 @@ public class MenuActivity extends PreferenceActivity implements OnSharedPreferen
         mUserPrefs = new UserPrefs( this );
         mUserPrefs.enforceLocale( this );
         mPrefs = PreferenceManager.getDefaultSharedPreferences( this );
-        RomDetail.initializeDatabase( mAppData.mupen64plus_ini );
         
         int lastVer = mAppData.getLastAppVersionCode();
         int currVer = mAppData.appVersionCode;
@@ -121,51 +101,13 @@ public class MenuActivity extends PreferenceActivity implements OnSharedPreferen
             }
         }
         
-        // Disable the Xperia PLAY plugin as necessary
-        if( !mAppData.hardwareInfo.isXperiaPlay )
-            mPrefs.edit().putBoolean( TOUCHPAD_ENABLED, false ).commit();
-        
-        // Set some prefs when running in big-screen mode
-        if( mUserPrefs.isBigScreenMode )
-        {
-            mPrefs.edit().putBoolean( TOUCHSCREEN_ENABLED, false ).commit();
-            mPrefs.edit().putBoolean( INPUT_VOLUME_MAPPABLE, true ).commit();
-        }
-        
-        // Ensure that any missing preferences are populated with defaults (e.g. preference added to new release)
-        PreferenceManager.setDefaultValues( this, R.xml.preferences, false );
-        PreferenceManager.setDefaultValues( this, R.xml.preferences_global, false );
-        PreferenceManager.setDefaultValues( this, R.xml.preferences_play, false );
-        PreferenceManager.setDefaultValues( this, R.xml.preferences_video, false );
-        
-        // Ensure that selected plugin names and other list preferences are valid
-        Resources res = getResources();
-        PrefUtil.validateListPreference( res, mPrefs, TOUCHSCREEN_STYLE, R.string.touchscreenStyle_default, R.array.touchscreenStyle_values );
-        PrefUtil.validateListPreference( res, mPrefs, TOUCHSCREEN_HEIGHT, R.string.touchscreenHeight_default, R.array.touchscreenHeight_values );
-        PrefUtil.validateListPreference( res, mPrefs, TOUCHSCREEN_LAYOUT, R.string.touchscreenLayout_default, R.array.touchscreenLayout_values );
-        PrefUtil.validateListPreference( res, mPrefs, TOUCHPAD_LAYOUT, R.string.touchpadLayout_default, R.array.touchpadLayout_values );
-        PrefUtil.validateListPreference( res, mPrefs, DISPLAY_POSITION, R.string.displayPosition_default, R.array.displayPosition_values );
-        PrefUtil.validateListPreference( res, mPrefs, DISPLAY_RESOLUTION, R.string.displayResolution_default, R.array.displayResolution_values );
-        PrefUtil.validateListPreference( res, mPrefs, DISPLAY_SCALING, R.string.displayScaling_default, R.array.displayScaling_values );
-        PrefUtil.validateListPreference( res, mPrefs, AUDIO_PLUGIN, R.string.audioPlugin_default, R.array.audioPlugin_values );
-        PrefUtil.validateListPreference( res, mPrefs, AUDIO_BUFFER_SIZE, R.string.audioBufferSize_default, R.array.audioBufferSize_values );
-        PrefUtil.validateListPreference( res, mPrefs, R4300_EMULATOR, R.string.r4300Emulator_default, R.array.r4300Emulator_values );
-        PrefUtil.validateListPreference( res, mPrefs, NAVIGATION_MODE, R.string.navigationMode_default, R.array.navigationMode_values );
-        
         // Load user preference menu structure from XML and update view
         addPreferencesFromResource( R.xml.preferences );
-        
-        // Refresh the preference data wrapper
-        mUserPrefs = new UserPrefs( this );
         
         // Populate the language menu
         ListPreference languagePref = (ListPreference) findPreference( LOCALE_OVERRIDE );
         languagePref.setEntryValues( mUserPrefs.localeCodes );
         languagePref.setEntries( mUserPrefs.localeNames );
-        
-        // Initialize the OUYA interface if running on OUYA
-        if( OUYAInterface.IS_OUYA_HARDWARE )
-            OUYAInterface.init( this );
         
         // Popup a warning if the installation appears to be corrupt
         if( !mAppData.isValidInstallation )

commit 4060b88db3d0e3b834e94d43fd30f4f76312743c

front: Fix bug where touchpad could be unintentionally enabled.
Bug introduced in commit
17539be993ccbd44d3ff60b0e3423a3b287e96f6
and exacerbated when global prefs menu added to PlayMenuActivity.

Bug occurs when user resets global preferences.  If they launch a game
before restarting the app, the touchpad would always be enabled, since
the hardware test to disable it for non-Xperia-PLAY devices only hap-
pens in SplashActivity.

The bug breaks analog controls when using a peripheral controller,
because GameActivityXperiaPlay ignores joystick events. (It only
listens to touch and button events.)  There might be a performance
difference between the GameActivity* variants as well.

src/paulscode/android/mupen64plusae/SplashActivity.java
@@ -86,7 +86,6 @@ public class SplashActivity extends Activity implements OnExtractionProgressList
     private static final String TOUCHSCREEN_STYLE = "touchscreenStyle";
     private static final String TOUCHSCREEN_HEIGHT = "touchscreenHeight";
     private static final String TOUCHSCREEN_LAYOUT = "touchscreenLayout";
-    private static final String TOUCHPAD_ENABLED = "touchpadEnabled";
     private static final String TOUCHPAD_LAYOUT = "touchpadLayout";
     private static final String INPUT_VOLUME_MAPPABLE = "inputVolumeMappable";
     private static final String DISPLAY_POSITION = "displayPosition";
@@ -117,10 +116,6 @@ public class SplashActivity extends Activity implements OnExtractionProgressList
         Uri dataUri = this.getIntent().getData();
         if( dataUri != null )
             mUserPrefs.putPathSelectedGame( dataUri.getPath() );
-        
-        // Disable the Xperia PLAY plugin as necessary
-        if( !mAppData.hardwareInfo.isXperiaPlay )
-            mPrefs.edit().putBoolean( TOUCHPAD_ENABLED, false ).commit();
         
         // Set some prefs when running in big-screen mode
         if( mUserPrefs.isBigScreenMode )

src/paulscode/android/mupen64plusae/persistent/UserPrefs.java
@@ -446,7 +446,7 @@ public class UserPrefs
         isTouchscreenHidden = transparencyPercent == 0;
         
         // Xperia PLAY touchpad prefs
-        isTouchpadEnabled = mPreferences.getBoolean( "touchpadEnabled", false );
+        isTouchpadEnabled = appData.hardwareInfo.isXperiaPlay && mPreferences.getBoolean( "touchpadEnabled", true );
         isTouchpadFeedbackEnabled = mPreferences.getBoolean( "touchpadFeedback", false );
         touchpadLayout = appData.touchpadLayoutsDir + mPreferences.getString( "touchpadLayout", "" );
         