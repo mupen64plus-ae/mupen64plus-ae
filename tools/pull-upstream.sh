55f05e223126729f42b4f36ca7f7d76f6f5e0648
littleguy77 <mattlichter@live.com>
2014-01-28 17:32:01 -0500

    tools: Add pull-upstream script.

---
+++ tools/pull-upstream.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+# This is more an example how to merge upstream sources directly than an
+# actual script because it breaks quite easily when detecting a merge problem.
+#
+# If you don't have git subtree, get it here:
+# From https://github.com/git/git/blob/master/contrib/subtree/git-subtree.sh
+# curl https://raw.github.com/git/git/master/contrib/subtree/git-subtree.sh > git-subtree.sh
+# GITSUBTREE=./tools/git-subtree.sh
+
+set -e
+cd ..
+
+COMPONENTS_ALL="audio-sdl core rsp-hle ui-console video-glide64mk2 video-rice"
+
+echo
+echo "Type the names of the upstream repositories you wish to pull, separated by whitespace."
+echo "Choices: ${COMPONENTS_ALL}"
+echo
+read COMPONENTS
+
+for i in $COMPONENTS; do
+    git subtree pull --prefix="jni/mupen64plus-${i}" --message="${i}: Sync with upstream." https://github.com/mupen64plus-ae/mupen64plus-"${i}".git master
+done
+
+echo "Finished"
