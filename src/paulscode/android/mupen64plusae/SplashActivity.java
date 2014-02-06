aca7d21cb6aabc1f43099fc2fd430449ff09351e
littleguy77 <mattlichter@live.com>
2014-02-05 22:29:02 -0500

    front: Change emulation profile names.
    Using more descriptive names for profiles (Accuracy, Balanced, Speed).

--- src/paulscode/android/mupen64plusae/SplashActivity.java
+++ src/paulscode/android/mupen64plusae/SplashActivity.java
@@ -56,7 +56,7 @@ public class SplashActivity extends Activity implements OnExtractionProgressList
      * Asset version number, used to determine stale assets. Increment this number every time the
      * assets are updated on disk.
      */
-    private static final int ASSET_VERSION = 37;
+    private static final int ASSET_VERSION = 38;
     
     /** The total number of assets to be extracted (for computing progress %). */
     private static final int TOTAL_ASSETS = 124;
