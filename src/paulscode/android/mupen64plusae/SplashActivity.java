e45ae9dc1d1b37474d041538a68034cb12438871
littleguy77 <mattlichter@live.com>
2014-02-15 22:41:05 -0500

    assets: Fix duplicate entries and broken formatting in mupencheat.txt.
    Also whitespace nits.  Already submitted a pull request to upstream.

--- src/paulscode/android/mupen64plusae/SplashActivity.java
+++ src/paulscode/android/mupen64plusae/SplashActivity.java
@@ -57,7 +57,7 @@ public class SplashActivity extends Activity implements OnExtractionProgressList
      * Asset version number, used to determine stale assets. Increment this number every time the
      * assets are updated on disk.
      */
-    private static final int ASSET_VERSION = 40;
+    private static final int ASSET_VERSION = 41;
     
     /** The total number of assets to be extracted (for computing progress %). */
     private static final int TOTAL_ASSETS = 124;
