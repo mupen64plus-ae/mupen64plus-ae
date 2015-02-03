4d6a0b4a33a14b17f1e9bfcddb188e20a99637a0
littleguy77 <mattlichter@live.com>
2015-02-02 22:47:10 -0500

    tools: Fix transifex pull script.
    Not sure why, but the unix2dos conversions seem to mess up the encoding
    of the files.  Looks like a unicode thing, perhaps due to Transifex
    updates.

--- tools/pull-translations.sh
+++ tools/pull-translations.sh
@@ -8,14 +8,5 @@ pattern='./res/values-*/strings.xml ./doc/publish/listing-*.txt'
 git pull
 tx pull -a -f
 
-environment=`uname -o`
-if [ "$environment" = "Cygwin" -o "$environment" = "Msys" ]; then
-    echo "Converting line endings to CRLF"
-    unix2dos $pattern
-else
-    echo "Converting line endings to LF"
-    dos2unix $pattern
-fi
-
 git add $pattern
 git commit $pattern -m "res: Updated translations."
