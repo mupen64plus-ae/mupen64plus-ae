0c86a75c4f8b29e614b224ceebe3e22f2ed01a0e
littleguy77 <mattlichter@live.com>
2015-04-20 09:58:04 -0400

    tools: Tweak pull-upstream script to handle ancestry failures.

--- tools/pull-upstream.sh
+++ tools/pull-upstream.sh
@@ -101,7 +101,11 @@ for i in $COMPONENTS; do
     NEW_SYNC_HASH=`git log -1 --pretty=format:"%h"`
     NEW_MSG_SUBJECT="${MSG_PREFIX}${NEW_SYNC_HASH}."
     NEW_MSG_LINK="${COMMIT_URL}${NEW_SYNC_HASH}"
-    NEW_MSG_BODY=`git log ${OLD_SYNC_HASH}..${NEW_SYNC_HASH} --pretty=format:"%h %s" --graph`
+    if git log ${OLD_SYNC_HASH}..${NEW_SYNC_HASH}; then
+        NEW_MSG_BODY=`git log ${OLD_SYNC_HASH}..${NEW_SYNC_HASH} --pretty=format:"%h %s" --graph`
+    else
+        NEW_MSG_BODY="Not a direct descendant of base revision."
+    fi
     echo
     echo "...to commit ${NEW_SYNC_HASH}"
     echo 
