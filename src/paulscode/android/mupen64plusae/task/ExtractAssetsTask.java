98cab4292ca7a5b0b92f5256ebf8baa5adb2242b
Francisco Zurita <fzurita@gmail.com>
2016-08-06 23:16:43 -0400

    front: No longer splitting asset files.
    This was needed only for Android 2.2 and earlier which we no longer support.

--- src/paulscode/android/mupen64plusae/task/ExtractAssetsTask.java
+++ src/paulscode/android/mupen64plusae/task/ExtractAssetsTask.java
@@ -23,24 +23,19 @@
  */
 package paulscode.android.mupen64plusae.task;
 
+import android.content.res.AssetManager;
+import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Log;
+
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import android.content.res.AssetManager;
-import android.os.AsyncTask;
-import android.text.TextUtils;
-import android.util.Log;
 
 public class ExtractAssetsTask extends AsyncTask<Void, String, List<ExtractAssetsTask.Failure>>
 {
@@ -148,29 +143,12 @@ public class ExtractAssetsTask extends AsyncTask<Void, String, List<ExtractAsset
             // Ensure the parent directories exist
             new File( dstPath ).mkdirs();
             
-            // Some files are too big for Android 2.2 and below, so we break them into parts.
-            // We use a simple naming scheme where we just append .part0, .part1, etc.
-            Pattern pattern = Pattern.compile( "(.+)\\.part(\\d+)$" );
-            HashMap<String, Integer> fileParts = new HashMap<String, Integer>();
-            
             // Recurse into each subdirectory
             for( String srcSubPath : srcSubPaths )
             {
-                Matcher matcher = pattern.matcher( srcSubPath );
-                if( matcher.matches() )
-                {
-                    String name = matcher.group(1);
-                    if( fileParts.containsKey( name ) )
-                        fileParts.put( name, fileParts.get( name ) + 1 );
-                    else
-                        fileParts.put( name, 1 );
-                }
                 String suffix = "/" + srcSubPath;
                 failures.addAll( extractAssets( srcPath + suffix, dstPath + suffix ) );
             }
-            
-            // Combine the large broken files, if any
-            combineFileParts( fileParts, dstPath );
         }
         else // srcPath is a file.
         {
@@ -255,82 +233,5 @@ public class ExtractAssetsTask extends AsyncTask<Void, String, List<ExtractAsset
         }
         
         return srcSubPaths;
-    }
-
-    private static List<Failure> combineFileParts( Map<String, Integer> filePieces, String dstPath )
-    {
-        List<Failure> failures = new ArrayList<Failure>();
-        for (String name : filePieces.keySet() )
-        {
-            String src = null;
-            String dst = dstPath + "/" + name;
-            OutputStream out = null;
-            InputStream in = null;
-            try
-            {
-                out = new FileOutputStream( dst );
-                byte[] buffer = new byte[1024];
-                int read;
-                for( int i = 0; i < filePieces.get( name ); i++ )
-                {
-                    src = dst + ".part" + i;
-                    try
-                    {
-                        in = new FileInputStream( src );
-                        while( ( read = in.read( buffer ) ) != -1 )
-                        {
-                            out.write( buffer, 0, read );
-                        }
-                        out.flush();
-                    }
-                    catch( IOException e )
-                    {
-                        Failure failure = new Failure( src, dst, Failure.Reason.FILE_IO_EXCEPTION ); 
-                        Log.e( "ExtractAssetsTask", failure.toString() );
-                        failures.add( failure );
-                    }
-                    finally
-                    {
-                        if( in != null )
-                        {
-                            try
-                            {
-                                in.close();
-                                new File( src ).delete();
-                            }
-                            catch( IOException e )
-                            {
-                                Failure failure = new Failure( src, dst, Failure.Reason.FILE_UNCLOSABLE ); 
-                                Log.e( "ExtractAssetsTask", failure.toString() );
-                                failures.add( failure );
-                            }
-                        }
-                    }
-                }
-            }
-            catch( FileNotFoundException e )
-            {
-                Failure failure = new Failure( src, dst, Failure.Reason.FILE_UNWRITABLE ); 
-                Log.e( "ExtractAssetsTask", failure.toString() );
-                failures.add( failure );
-            }
-            finally
-            {
-                if( out != null )
-                {
-                    try
-                    {
-                        out.close();
-                    }
-                    catch( IOException e )
-                    {
-                        Failure failure = new Failure( src, dst, Failure.Reason.FILE_UNCLOSABLE ); 
-                        Log.e( "ExtractAssetsTask", failure.toString() );
-                        failures.add( failure );
-                    }
-                }
-            }
-        }
-        return failures;
     }
 }
