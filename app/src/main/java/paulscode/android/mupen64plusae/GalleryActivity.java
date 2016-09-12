2b816c2b765ae296929c7041f514e563828e14f8
Francisco Zurita <fzurita@gmail.com>
2016-09-12 19:51:39 -0400

    front: Better way closing drawer without animation
    This fixes https://github.com/mupen64plus-ae/mupen64plus-ae/issues/580

--- app/src/main/java/paulscode/android/mupen64plusae/GalleryActivity.java
+++ app/src/main/java/paulscode/android/mupen64plusae/GalleryActivity.java
@@ -44,7 +44,6 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.view.inputmethod.InputMethodManager;
 
 import org.mupen64plusae.v3.alpha.R;
@@ -85,7 +84,6 @@ public class GalleryActivity extends AppCompatActivity implements GameSidebarAct
     private static final String STATE_CACHE_ROM_INFO_FRAGMENT= "STATE_CACHE_ROM_INFO_FRAGMENT";
     private static final String STATE_EXTRACT_TEXTURES_FRAGMENT= "STATE_EXTRACT_TEXTURES_FRAGMENT";
     private static final String STATE_GALLERY_REFRESH_NEEDED= "STATE_GALLERY_REFRESH_NEEDED";
-    private static final String STATE_CLOSE_SIDEBAR_NEEDED= "STATE_CLOSE_SIDEBAR_NEEDED";
     private static final String STATE_RESTART_CONFIRM_DIALOG = "STATE_RESTART_CONFIRM_DIALOG";
     private static final String STATE_CLEAR_CONFIRM_DIALOG = "STATE_CLEAR_CONFIRM_DIALOG";
     private static final int RESTART_CONFIRM_DIALOG_ID = 0;
@@ -126,9 +124,6 @@ public class GalleryActivity extends AppCompatActivity implements GameSidebarAct
 
     //If this is set to true, the gallery will be refreshed next time this activity is resumed
     boolean mRefreshNeeded = false;
-
-    //The side bar will be closed next time the Gallery Activity is resumed
-    boolean mCloseSidebarNeeded = false;
 
     @Override
     protected void onNewIntent( Intent intent )
@@ -331,7 +326,6 @@ public class GalleryActivity extends AppCompatActivity implements GameSidebarAct
                 mSearchQuery = query;
 
             mRefreshNeeded = savedInstanceState.getBoolean(STATE_GALLERY_REFRESH_NEEDED);
-            mCloseSidebarNeeded = savedInstanceState.getBoolean(STATE_CLOSE_SIDEBAR_NEEDED);
         }
 
         // find the retained fragment on activity restarts
@@ -372,29 +366,6 @@ public class GalleryActivity extends AppCompatActivity implements GameSidebarAct
             mGameSidebar.setVisibility( View.GONE );
             mDrawerList.setVisibility( View.VISIBLE );
         }
-
-        if(mCloseSidebarNeeded)
-        {
-            //Close the drawer without an animation
-            final View view = mDrawerLayout.getChildAt(mDrawerLayout.getChildCount() - 1);
-            final ViewTreeObserver vto = view.getViewTreeObserver();
-
-            vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener()
-            {
-                @Override
-                public boolean onPreDraw()
-                {
-                    final DrawerLayout.LayoutParams lp = new DrawerLayout.LayoutParams(view.getWidth(), view
-                            .getHeight());
-                    lp.gravity = GravityCompat.START;
-                    view.setLayoutParams(lp);
-                    view.setLeft(-view.getMeasuredWidth());
-                    view.getViewTreeObserver().removeOnPreDrawListener(this);
-
-                    return true;
-                }
-            });
-        }
     }
 
     @Override
@@ -405,7 +376,6 @@ public class GalleryActivity extends AppCompatActivity implements GameSidebarAct
         if( mSelectedItem != null )
             savedInstanceState.putString( STATE_SIDEBAR, mSelectedItem.md5 );
         savedInstanceState.putBoolean(STATE_GALLERY_REFRESH_NEEDED, mRefreshNeeded);
-        savedInstanceState.putBoolean(STATE_CLOSE_SIDEBAR_NEEDED, mCloseSidebarNeeded);
 
         super.onSaveInstanceState( savedInstanceState );
     }
@@ -1020,7 +990,9 @@ public class GalleryActivity extends AppCompatActivity implements GameSidebarAct
             config.save();
         }
 
-        mCloseSidebarNeeded = true;
+        //Close drawer without animation
+        mDrawerLayout.closeDrawer( GravityCompat.START, false );
+        
         mSelectedItem = null;
         
         String romLegacySaveFileName;
