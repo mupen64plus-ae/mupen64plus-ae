7f27e204a495c22f32d74340ca294aa9ef105e9f
Francisco Zurita <fzurita@gmail.com>
2016-10-03 19:57:58 -0400

    front: During ROM scan, if saved path is a file, restore parent directory.

--- app/src/main/java/paulscode/android/mupen64plusae/ScanRomsActivity.java
+++ app/src/main/java/paulscode/android/mupen64plusae/ScanRomsActivity.java
@@ -70,6 +70,11 @@ public class ScanRomsActivity extends AppCompatActivity implements OnItemClickLi
             else
             {
                 mCurrentPath = new File( romScanStartPath );
+
+                if(mCurrentPath.isFile())
+                {
+                    mCurrentPath = mCurrentPath.getParentFile();
+                }
             }
         }
          
