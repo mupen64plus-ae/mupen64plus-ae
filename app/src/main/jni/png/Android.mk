83d199203a50d6bb0ca7f6d0ea8330d4e0258e31
Francisco Zurita <fzurita@gmail.com>
2017-03-08 01:35:47 -0500

    build: Update libpng to 1.6.23

--- app/src/main/jni/png/Android.mk
+++ app/src/main/jni/png/Android.mk
@@ -1,31 +1,30 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
-LOCAL_MODULE:= png
+LOCAL_CFLAGS :=
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_MODULE    := libpng
+LOCAL_SRC_FILES :=\
+	png.c \
+	pngerror.c \
+	pngget.c \
+	pngmem.c \
+	pngpread.c \
+	pngread.c \
+	pngrio.c \
+	pngrtran.c \
+	pngrutil.c \
+	pngset.c \
+	pngtest.c \
+	pngtrans.c \
+	pngwio.c \
+	pngwrite.c \
+	pngwtran.c \
+	pngwutil.c
 
-LOCAL_SRC_FILES :=  \
-    png.c           \
-    pngerror.c      \
-    pngget.c        \
-    pngmem.c        \
-    pngpread.c      \
-    pngread.c       \
-    pngrio.c        \
-    pngrtran.c      \
-    pngrutil.c      \
-    pngset.c        \
-    pngtest.c       \
-    pngtrans.c      \
-    pngwio.c        \
-    pngwrite.c      \
-    pngwtran.c      \
-    pngwutil.c      \
-
-LOCAL_CFLAGS := -O3
-
+#LOCAL_SHARED_LIBRARIES := -lz
 LOCAL_EXPORT_LDLIBS := -lz
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/.
 
+#include $(BUILD_SHARED_LIBRARY)
 include $(BUILD_STATIC_LIBRARY)
