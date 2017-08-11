b36392a91bf6c5fbe2739a4f87c93672fc6c5f1e
Justin Michaud <justin@justinmichaud.com>
2017-08-10 22:01:11 -0400

    kinda works

--- app/src/main/jni/mupen64plus-video-rice/src/Render.cpp
+++ app/src/main/jni/mupen64plus-video-rice/src/Render.cpp
@@ -49,7 +49,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #undef max
 
 /////////////////////////////////////
-#define ASENSOR_TYPE_ROTATION_VECTOR 11
+#define ASENSOR_TYPE_ROTATION_VECTOR 15
 #include <android/looper.h>
 #include <android/sensor.h>
 
@@ -118,6 +118,60 @@ int SetupSensor() {
     return 0;
 }
 
+const int AXIS_X = 1;
+const int AXIS_Y = 2;
+const int AXIS_Z = 3;
+const int AXIS_MINUS_X = AXIS_X | 0x80;
+const int AXIS_MINUS_Y = AXIS_Y | 0x80;
+const int AXIS_MINUS_Z = AXIS_Z | 0x80;
+
+bool remapCoordinateSystem(const XMATRIX &inR, const int X, const int Y, XMATRIX &outR)
+{
+    /*
+     * X and Y define a rotation matrix 'r':
+     *
+     *  (X==1)?((X&0x80)?-1:1):0    (X==2)?((X&0x80)?-1:1):0    (X==3)?((X&0x80)?-1:1):0
+     *  (Y==1)?((Y&0x80)?-1:1):0    (Y==2)?((Y&0x80)?-1:1):0    (Y==3)?((X&0x80)?-1:1):0
+     *                              r[0] ^ r[1]
+     *
+     * where the 3rd line is the vector product of the first 2 lines
+     *
+     */
+    if ((X & 0x7C)!=0 || (Y & 0x7C)!=0)
+        return false;   // invalid parameter
+    if (((X & 0x3)==0) || ((Y & 0x3)==0))
+        return false;   // no axis specified
+    if ((X & 0x3) == (Y & 0x3))
+        return false;   // same axis specified
+    // Z is "the other" axis, its sign is either +/- sign(X)*sign(Y)
+    // this can be calculated by exclusive-or'ing X and Y; except for
+    // the sign inversion (+/-) which is calculated below.
+    int Z = X ^ Y;
+    // extract the axis (remove the sign), offset in the range 0 to 2.
+    const int x = (X & 0x3)-1;
+    const int y = (Y & 0x3)-1;
+    const int z = (Z & 0x3)-1;
+    // compute the sign of Z (whether it needs to be inverted)
+    const int axis_y = (z+1)%3;
+    const int axis_z = (z+2)%3;
+    if (((x^axis_y)|(y^axis_z)) != 0)
+        Z ^= 0x80;
+    const bool sx = (X>=0x80);
+    const bool sy = (Y>=0x80);
+    const bool sz = (Z>=0x80);
+    // Perform R * r, in avoiding actual muls and adds.
+    for (unsigned int j=0 ; j<3 ; j++) {
+        for (unsigned int i=0 ; i<3 ; i++) {
+            if (x==i)   outR(j, i) = sx ? -inR(j,0) : inR(j,0);
+            if (y==i)   outR(j, i) = sy ? -inR(j,1) : inR(j,1);
+            if (z==i)   outR(j, i) = sz ? -inR(j,2) : inR(j,2);
+        }
+    }
+    outR._14 = outR._24 = outR._34 = outR._41 = outR._42 = outR._43 = 0;
+    outR._44 = 1;
+    return true;
+}
+
 int pollForSensorData() {
     if (!VR_SENSOR_QUEUE || !VR_SENSOR) {
         LOGD("**************** Sensors not initialized\n");
@@ -141,9 +195,6 @@ int pollForSensorData() {
     float q3 = data[0].data[2];
     float q0 = data[0].data[3];
 
-    q0 = 1 - q1*q1 - q2*q2 - q3*q3;
-    q0 = (q0 > 0) ? (float)sqrt(q0) : 0;
-
     float sq_q1 = 2 * q1 * q1;
     float sq_q2 = 2 * q2 * q2;
     float sq_q3 = 2 * q3 * q3;
@@ -159,19 +210,22 @@ int pollForSensorData() {
     R._11 = 1 - sq_q2 - sq_q3;
     R._12 = q1_q2 - q3_q0;
     R._13 = q1_q3 + q2_q0;
-    R._14 = 0.0f;
     R._21 = q1_q2 + q3_q0;
     R._22 = 1 - sq_q1 - sq_q3;
     R._23 = q2_q3 - q1_q0;
-    R._24 = 0.0f;
     R._31 = q1_q3 - q2_q0;
     R._32 = q2_q3 + q1_q0;
     R._33 = 1 - sq_q1 - sq_q2;
-    R._34 = 0.0f;
 
-    XMATRIX BASIS(0,1,0,0, 0,0,1,0, 1,0,0,0, 0,0,0,1);
+    VR_TRANSFORM_MAT = XMATRIX(1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1);
+    remapCoordinateSystem(R, AXIS_Y, AXIS_MINUS_X, VR_TRANSFORM_MAT);
 
-    VR_TRANSFORM_MAT = BASIS*R*BASIS;
+//    LOGD("************************ Quaternion matrix\n");
+//    LOGD("************************ %f %f %f %f", R._11, R._12, R._13, R._14);
+//    LOGD("************************ %f %f %f %f", R._21, R._22, R._23, R._24);
+//    LOGD("************************ %f %f %f %f", R._31, R._32, R._33, R._34);
+//    LOGD("************************ %f %f %f %f", R._41, R._42, R._43, R._44);
+
     return 0;
 }
 
