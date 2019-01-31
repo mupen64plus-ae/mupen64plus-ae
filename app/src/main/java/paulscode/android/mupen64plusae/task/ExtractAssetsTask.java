705fa73fdf2d199e8afe877075dc55f4c3865045
Raphael Assenat <raph@raphnet.net>
2019-01-31 15:30:13 -0500

    Bump controller.cfg version number to 2

--- app/src/main/java/paulscode/android/mupen64plusae/task/ExtractAssetsTask.java
+++ app/src/main/java/paulscode/android/mupen64plusae/task/ExtractAssetsTask.java
@@ -89,7 +89,7 @@ public class ExtractAssetsTask extends AsyncTask<Void, String, List<ExtractAsset
             mAssetVersions.put("mupen64plus_data/gln64rom.conf", 1);
             mAssetVersions.put("mupen64plus_data/mupen64plus.ini", 4);
             mAssetVersions.put("mupen64plus_data/mupencheat.default", 1);
-            mAssetVersions.put("mupen64plus_data/profiles/controller.cfg", 1);
+            mAssetVersions.put("mupen64plus_data/profiles/controller.cfg", 2);
             mAssetVersions.put("mupen64plus_data/profiles/emulation.cfg", 3);
             mAssetVersions.put("mupen64plus_data/profiles/touchscreen.cfg", 2);
             mAssetVersions.put("mupen64plus_data/skins/touchscreen/JoshaGibs/analog-back.png", 1);
