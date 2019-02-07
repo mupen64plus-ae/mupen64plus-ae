02144b11162726654fc2029f59c5c794f58f11fb
fzurita <fzurita@gmail.com>
2019-02-07 08:29:04 -0500

    build: Incorporate libusb and libhdmi libraries

--- mupen64plus-input-raphnet/Android.mk
+++ mupen64plus-input-raphnet/Android.mk
@@ -1,4 +1,6 @@
 JNI_LOCAL_PATH := $(call my-dir)
 include $(JNI_LOCAL_PATH)/../build_common/native_common.mk
+include $(JNI_LOCAL_PATH)/libusb/android/jni/Android.mk
+include $(JNI_LOCAL_PATH)/hidapi/android/jni/Android.mk
 include $(JNI_LOCAL_PATH)/upstream/src/Android.mk
 
