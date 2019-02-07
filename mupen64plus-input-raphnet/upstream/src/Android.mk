5ef80e157ba0e2b4a240e305bafab435eee1bf71
fzurita <fzurita@gmail.com>
2019-02-07 08:29:04 -0500

    build: Incorporate libusb and libhdmi libraries

--- mupen64plus-input-raphnet/upstream/src/Android.mk
+++ mupen64plus-input-raphnet/upstream/src/Android.mk
@@ -2,9 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := mupen64plus-input-raphnet
 LOCAL_C_INCLUDES := $(M64P_API_INCLUDES)
+MY_LOCAL_SHARED_LIBRARIES := libhidapi
+
 LOCAL_SRC_FILES := plugin.c
+
+LOCAL_MODULE := mupen64plus-input-raphnet
 LOCAL_CFLAGS := $(COMMON_CFLAGS)
 LOCAL_LDFLAGS := $(COMMON_LDFLAGS)
 LOCAL_LDLIBS := -llog
