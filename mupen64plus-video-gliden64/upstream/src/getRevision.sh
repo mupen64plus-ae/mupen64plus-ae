f7cdbdf9911100ea023c11cee39043b747858155
fzurita <fzurita@gmail.com>
2019-12-14 16:33:29 -0500

    Don't regenerate GLideN64 Revision.h if it hasn't changed

--- mupen64plus-video-gliden64/upstream/src/getRevision.sh
+++ mupen64plus-video-gliden64/upstream/src/getRevision.sh
@@ -1,5 +1,13 @@
 SCRIPT_DIRECTORY=`dirname $0`
 rev=\"`git rev-parse --short HEAD`\"
+lastrev=$(cat $SCRIPT_DIRECTORY/revision.txt)
+
 echo current revision $rev
-echo "#define PLUGIN_REVISION $rev" > $SCRIPT_DIRECTORY/Revision.h
-echo "#define PLUGIN_REVISION_W L$rev" >> $SCRIPT_DIRECTORY/Revision.h
+echo last build revision $lastrev
+
+if [ "$lastrev" != "$rev" ]
+then
+   echo "#define PLUGIN_REVISION $rev" > $SCRIPT_DIRECTORY/Revision.h
+   echo "#define PLUGIN_REVISION_W L$rev" >> $SCRIPT_DIRECTORY/Revision.h
+   echo "$rev" > $SCRIPT_DIRECTORY/revision.txt
+fi
