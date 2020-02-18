ee866bd6cc87ca907a44d5919bfed0d585970cc3
fzurita <fzurita@gmail.com>
2020-02-17 21:18:52 -0500

    Increase timeout for core shutdown
    This can sometimes take longer than expected

--- app/src/main/java/paulscode/android/mupen64plusae/jni/CoreService.java
+++ app/src/main/java/paulscode/android/mupen64plusae/jni/CoreService.java
@@ -978,7 +978,7 @@ public class CoreService extends Service implements CoreInterface.OnFpsChangedLi
             long seconds = System.currentTimeMillis() / 1000L;
 
             //Use a 5 second timeout to save before killing the core process
-            if(seconds - mLastFpsChangedTime > 5)
+            if(seconds - mLastFpsChangedTime > 15)
             {
                 Log.e(TAG, "Killing Core due to no response");
                 forceExit();
