b7d7d4f45a8d0f946063f3effb80cdb5b76e6438
fzurita <fzurita@gmail.com>
2020-02-22 11:34:01 -0500

    git subrepo clone --branch=master --force https://github.com/mupen64plus-ae/mupen64plus-core.git mupen64plus-core/upstream
    subrepo:
      subdir:   "mupen64plus-core/upstream"
      merged:   "ecb69be98"
    upstream:
      origin:   "https://github.com/mupen64plus-ae/mupen64plus-core.git"
      branch:   "master"
      commit:   "ecb69be98"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "5d6aba9"

--- mupen64plus-core/upstream/src/device/rcp/pi/pi_controller.c
+++ mupen64plus-core/upstream/src/device/rcp/pi/pi_controller.c
@@ -52,13 +52,11 @@ enum
 
 /** static (local) variables **/
 static uint32_t   l_ForceAlignmentOfPiDmaCartMask = ~UINT32_C(1);
-static uint32_t   l_ForceAlignmentOfPiDmaDramMask = ~UINT32_C(7);
-
 
 static void dma_pi_read(struct pi_controller* pi)
 {
     uint32_t cart_addr = pi->regs[PI_CART_ADDR_REG] & l_ForceAlignmentOfPiDmaCartMask;
-    uint32_t dram_addr = pi->regs[PI_DRAM_ADDR_REG] & l_ForceAlignmentOfPiDmaDramMask;
+    uint32_t dram_addr = pi->regs[PI_DRAM_ADDR_REG];
     uint32_t length = (pi->regs[PI_RD_LEN_REG] & UINT32_C(0x00fffffe)) + 2;
     const uint8_t* dram = (uint8_t*)pi->ri->rdram->dram;
 
@@ -87,7 +85,7 @@ static void dma_pi_read(struct pi_controller* pi)
 static void dma_pi_write(struct pi_controller* pi)
 {
     uint32_t cart_addr = pi->regs[PI_CART_ADDR_REG] & l_ForceAlignmentOfPiDmaCartMask;
-    uint32_t dram_addr = pi->regs[PI_DRAM_ADDR_REG] & l_ForceAlignmentOfPiDmaDramMask;
+    uint32_t dram_addr = pi->regs[PI_DRAM_ADDR_REG];
     uint32_t length = (pi->regs[PI_WR_LEN_REG] & UINT32_C(0x00fffffe)) + 2;
     uint8_t* dram = (uint8_t*)pi->ri->rdram->dram;
 
@@ -126,7 +124,6 @@ void init_pi(struct pi_controller* pi,
     if(!forceAlignmentOfPiDma)
     {
         l_ForceAlignmentOfPiDmaCartMask = UINT32_C(0xffffffff);
-        l_ForceAlignmentOfPiDmaDramMask = UINT32_C(0xffffffff);
     }
 
     pi->get_pi_dma_handler = get_pi_dma_handler;
--- mupen64plus-core/upstream/src/main/savestates.c
+++ mupen64plus-core/upstream/src/main/savestates.c
@@ -484,7 +484,7 @@ static int savestates_load_m64p(struct device* dev, char *filepath)
     savestates_load_set_pc(&dev->r4300, GETDATA(curr, uint32_t));
 
     *r4300_cp0_next_interrupt(&dev->r4300.cp0) = GETDATA(curr, uint32_t);
-    dev->vi.next_vi = GETDATA(curr, uint32_t);
+    curr += 4; /* here there used to be next_vi */
     dev->vi.field = GETDATA(curr, uint32_t);
 
     // assert(savestateData+savestateSize == curr)
@@ -1011,7 +1011,7 @@ static int savestates_load_pj64(struct device* dev,
     *r4300_cp0_next_interrupt(&dev->r4300.cp0) = (cp0_regs[CP0_COMPARE_REG] < vi_timer)
                   ? cp0_regs[CP0_COMPARE_REG]
                   : vi_timer;
-    dev->vi.next_vi = vi_timer;
+
     dev->vi.field = 0;
     *((unsigned int*)&buffer[0]) = VI_INT;
     *((unsigned int*)&buffer[4]) = vi_timer;
@@ -1753,7 +1753,7 @@ static int savestates_save_m64p(const struct device* dev, char *filepath)
     PUTDATA(curr, uint32_t, *r4300_pc((struct r4300_core*)&dev->r4300));
 
     PUTDATA(curr, uint32_t, *r4300_cp0_next_interrupt((struct cp0*)&dev->r4300.cp0));
-    PUTDATA(curr, uint32_t, dev->vi.next_vi);
+    PUTDATA(curr, uint32_t, 0); /* here there used to be next_vi */
     PUTDATA(curr, uint32_t, dev->vi.field);
 
     to_little_endian_buffer(queue, 4, sizeof(queue)/4);
