94a66936b7f6bf540c302cc9b01ff4929905c06e
fzurita <fzurita@gmail.com>
2020-11-20 23:48:23 -0500

    Convert mupen64plus-audio-sles project to a CMake project
    Also, bring in the google oboe library and cleanup legacy NDK make files

--- build_common/native_common.mk
+++ build_common/native_common.mk
@@ -11,13 +11,6 @@ LOCAL_MODULE := libhidapi
 LOCAL_SRC_FILES := $(JNI_LOCAL_PATH)/../ndkLibs/libs/$(BUILD_VARIANT)/$(TARGET_ARCH_ABI)/libhidapi.so
 include $(PREBUILT_SHARED_LIBRARY)
 
-#SDL2
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := SDL2
-#LOCAL_SRC_FILES := $(JNI_LOCAL_PATH)/../ndkLibs/libs/$(BUILD_VARIANT)/$(TARGET_ARCH_ABI)/libSDL2.so
-#LOCAL_EXPORT_C_INCLUDES := $(JNI_LOCAL_PATH)/../ndkLibs/SDL2/include/
-#include $(PREBUILT_SHARED_LIBRARY)
-
 #PNG
 include $(CLEAR_VARS)
 LOCAL_MODULE := png
@@ -26,33 +19,12 @@ LOCAL_EXPORT_C_INCLUDES := $(JNI_LOCAL_PATH)/../ndkLibs/png/
 LOCAL_EXPORT_LDLIBS := -lz
 include $(PREBUILT_STATIC_LIBRARY)
 
-#Soundtouch
-include $(CLEAR_VARS)
-LOCAL_MODULE := soundtouch
-LOCAL_SRC_FILES := $(JNI_LOCAL_PATH)/../ndkLibs/libs/$(BUILD_VARIANT)/$(TARGET_ARCH_ABI)/libsoundtouch.so
-LOCAL_EXPORT_C_INCLUDES := $(JNI_LOCAL_PATH)/../ndkLibs/soundtouch/include/
-include $(PREBUILT_SHARED_LIBRARY)
-
-#Soundtouch floating point
-include $(CLEAR_VARS)
-LOCAL_MODULE := soundtouch_fp
-LOCAL_SRC_FILES := $(JNI_LOCAL_PATH)/../ndkLibs/libs/$(BUILD_VARIANT)/$(TARGET_ARCH_ABI)/libsoundtouch_fp.so
-LOCAL_EXPORT_C_INCLUDES := $(JNI_LOCAL_PATH)/../ndkLibs/soundtouch/include/
-include $(PREBUILT_SHARED_LIBRARY)
-
 #GL
 include $(CLEAR_VARS)
 LOCAL_MODULE := EGLLoader
 LOCAL_SRC_FILES := $(JNI_LOCAL_PATH)/../ndkLibs/libs/$(BUILD_VARIANT)/$(TARGET_ARCH_ABI)/libEGLLoader.a
 LOCAL_EXPORT_C_INCLUDES := $(JNI_LOCAL_PATH)/../ndkLibs/GL
 include $(PREBUILT_STATIC_LIBRARY)
-
-#Freetype
-include $(CLEAR_VARS)
-LOCAL_MODULE := freetype
-LOCAL_SRC_FILES := $(JNI_LOCAL_PATH)/../ndkLibs/libs/$(BUILD_VARIANT)/$(TARGET_ARCH_ABI)/libfreetype.so
-LOCAL_EXPORT_C_INCLUDES := $(JNI_LOCAL_PATH)/../ndkLibs/freetype/include/
-include $(PREBUILT_SHARED_LIBRARY)
 
 AE_BRIDGE_INCLUDES := $(JNI_LOCAL_PATH)/ae-bridge/
 M64P_API_INCLUDES := $(JNI_LOCAL_PATH)/../mupen64plus-core/upstream/src/api/
--- mupen64plus-audio-sles/Android.mk
+++
@@ -1,3 +0,0 @@
-JNI_LOCAL_PATH := $(call my-dir)
-include $(JNI_LOCAL_PATH)/../build_common/native_common.mk
-include $(JNI_LOCAL_PATH)/src/Android.mk
