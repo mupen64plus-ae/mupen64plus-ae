60a0e2363bec1a5c317265948aa57a250b0f12e0
fzurita <fzurita@gmail.com>
2020-11-23 14:54:45 -0500

    No longer need Yoshi workaround

--- app/src/main/java/paulscode/android/mupen64plusae/task/ExtractAssetsOrCleanupTask.java
+++ app/src/main/java/paulscode/android/mupen64plusae/task/ExtractAssetsOrCleanupTask.java
@@ -61,7 +61,7 @@ public class ExtractAssetsOrCleanupTask
 
     static {
         synchronized (ExtractAssetsOrCleanupTask.class) {
-            mAssetVersions.put("mupen64plus_data/GLideN64.custom.ini", 8);
+            mAssetVersions.put("mupen64plus_data/GLideN64.custom.ini", 9);
             mAssetVersions.put("mupen64plus_data/Glide64mk2.ini", 2);
             mAssetVersions.put("mupen64plus_data/RiceVideoLinux.ini", 1);
             mAssetVersions.put("mupen64plus_data/m64p_test_rom.v64", 1);
