1f63efb2991f9aa2e168f7c594021118e4cf36eb
fzurita <fzurita@gmail.com>
2020-12-13 09:47:02 -0500

    Move all touchscreen buttons slightly closer to the center of the screen

--- app/src/main/java/paulscode/android/mupen64plusae/task/ExtractAssetsOrCleanupTask.java
+++ app/src/main/java/paulscode/android/mupen64plusae/task/ExtractAssetsOrCleanupTask.java
@@ -72,7 +72,7 @@ public class ExtractAssetsOrCleanupTask
             mAssetVersions.put("mupen64plus_data/mupencheat.default", 1);
             mAssetVersions.put("mupen64plus_data/profiles/controller.cfg", 3);
             mAssetVersions.put("mupen64plus_data/profiles/emulation.cfg", 6);
-            mAssetVersions.put("mupen64plus_data/profiles/touchscreen.cfg", 2);
+            mAssetVersions.put("mupen64plus_data/profiles/touchscreen.cfg", 5);
             mAssetVersions.put("mupen64plus_data/skins/touchscreen/JoshaGibs/analog-back.png", 1);
             mAssetVersions.put("mupen64plus_data/skins/touchscreen/JoshaGibs/analog-fore.png", 1);
             mAssetVersions.put("mupen64plus_data/skins/touchscreen/JoshaGibs/analog.png", 1);
