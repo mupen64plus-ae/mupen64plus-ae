87ba474b90823a6966fe0dc3ff0cc0afa2f2c80b
Jo Masse <jonathan.masse.urca@gmail.com>
2021-09-30 17:40:36 -0400

    Revert "Missing condition."
    This reverts commit 15f7a6ea02d0bdf1fd19e44599f2606d9f86be2b.

--- app/src/main/java/paulscode/android/mupen64plusae/profile/TouchscreenProfileActivity.java
+++ app/src/main/java/paulscode/android/mupen64plusae/profile/TouchscreenProfileActivity.java
@@ -374,13 +374,9 @@ public class TouchscreenProfileActivity extends AppCompatActivity implements OnT
 
     private void disableAssetNoRefresh( String assetName )
     {
-        // If has asset.
-        if( hasAsset(assetName) )
-        {
-            // Change the position of the asset to hide
-            mProfile.putInt( assetName + TAG_X, DISABLED_ASSET_POS );
-            mProfile.putInt( assetName + TAG_Y, DISABLED_ASSET_POS );
-        }
+        // Change the position of the asset to hide
+        mProfile.putInt( assetName + TAG_X, DISABLED_ASSET_POS );
+        mProfile.putInt( assetName + TAG_Y, DISABLED_ASSET_POS );
     }
     
     private void setNotHoldable( int n64Index, boolean holdable )
