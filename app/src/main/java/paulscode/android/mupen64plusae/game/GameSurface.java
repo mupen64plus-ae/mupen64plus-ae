33dabbc88c654cc4c16a0fe55291eccf266c3db3
fzurita <fzurita@gmail.com>
2021-12-22 07:46:51 -0500

    Check for null handler before attempting to take a screenshot.

--- app/src/main/java/paulscode/android/mupen64plusae/game/GameSurface.java
+++ app/src/main/java/paulscode/android/mupen64plusae/game/GameSurface.java
@@ -182,7 +182,9 @@ public class GameSurface extends SurfaceView implements SurfaceHolder.Callback
         Log.i(TAG, "takeScreenshot: " + directory + " " + filename);
         Notifier.showToast(mContext, R.string.toast_savingScreenshot);
 
-        mRenderThread.getHandler().sendScreenshotRequest(directory, filename);
+        if (mRenderThread.getHandler() != null) {
+            mRenderThread.getHandler().sendScreenshotRequest(directory, filename);
+        }
     }
 
     @Override
