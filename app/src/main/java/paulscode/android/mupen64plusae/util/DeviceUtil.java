c99a2e255d54b3459ba0c1f8a57f704df79186a6
fzurita <fzurita@gmail.com>
2022-04-09 14:51:19 -0400

    Catch possible security exception

--- app/src/main/java/paulscode/android/mupen64plusae/util/DeviceUtil.java
+++ app/src/main/java/paulscode/android/mupen64plusae/util/DeviceUtil.java
@@ -1,21 +1,21 @@
 /*
  * Mupen64PlusAE, an N64 emulator for the Android platform
- * 
+ *
  * Copyright (C) 2013 Paul Lamb
- * 
+ *
  * This file is part of Mupen64PlusAE.
- * 
+ *
  * Mupen64PlusAE is free software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * Mupen64PlusAE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with Mupen64PlusAE. If
  * not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Authors: Paul Lamb, lioncash, littleguy77
  */
 package paulscode.android.mupen64plusae.util;
@@ -47,19 +47,19 @@ public final class DeviceUtil
 {
     /**
      * Gets the hardware information from /proc/cpuinfo.
-     * 
+     *
      * @return The hardware string.
      */
     public static String getCpuInfo()
     {
         // From http://android-er.blogspot.com/2009/09/read-android-cpu-info.html
         String result = Utility.executeShellCommand( "/system/bin/cat", "/proc/cpuinfo" );
-        
+
         // Remove the serial number for privacy
         Pattern pattern = Pattern.compile( "^serial\\s*?:.*?$", Pattern.CASE_INSENSITIVE
                 | Pattern.MULTILINE );
         result = pattern.matcher( result ).replaceAll( "Serial : XXXX" );
-        
+
         // Additional information in android.os.Build may be useful
         result += "\n";
         result += "Board: " + Build.BOARD + "\n";
@@ -79,7 +79,11 @@ public final class DeviceUtil
         String deviceName = Settings.System.getString(resolver, "device_name");
 
         if (deviceName == null) {
-            deviceName = Settings.Secure.getString(resolver, "bluetooth_name");
+            try {
+                deviceName = Settings.Secure.getString(resolver, "bluetooth_name");
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
         }
 
         if (deviceName == null) {
@@ -88,7 +92,11 @@ public final class DeviceUtil
             if (manager != null) {
                 BluetoothAdapter adapter = manager.getAdapter();
                 if (adapter != null) {
-                    deviceName = adapter.getName();
+                    try {
+                        deviceName = adapter.getName();
+                    } catch (SecurityException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
