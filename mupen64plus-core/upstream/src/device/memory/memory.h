4a68423603a9eb64c67972c1a30b9e084a1cbb53
Lillie <austins355@yahoo.com>
2024-05-28 00:54:32 -0500

    Update memory.h

--- mupen64plus-core/upstream/src/device/memory/memory.h
+++ mupen64plus-core/upstream/src/device/memory/memory.h
@@ -30,7 +30,7 @@
 enum { RDRAM_16MB_SIZE = 0x1000000 };
 enum { RDRAM_8MB_SIZE = 0x800000 };
 enum { RDRAM_4MB_SIZE = 0x400000 };
-enum { CART_ROM_MAX_SIZE = 0x4000000 };
+enum { CART_ROM_MAX_SIZE = 0x10000000 };
 enum { DD_ROM_MAX_SIZE = 0x400000 };
 
 typedef void (*read32fn)(void*,uint32_t,uint32_t*);
